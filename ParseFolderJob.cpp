3d4d69f497dce99f784f93fa05ab1059f2236133
cb <christophe.beaudet@gmail.com>
2019-05-19 18:49:56 +0200

    ,

--- ParseFolderJob.cpp
+++ ParseFolderJob.cpp
@@ -12,29 +12,55 @@ ParseFolderJob::ParseFolderJob(const fs::path& path)
 {
 }
 
+// Done by hand with windows stuff because this is faster than fs::recursive_directory_iterator (2x)
 bool ParseFolderJob::execute()
 {
-    string extension;
-    auto   rec = fs::recursive_directory_iterator(m_path);
-    for (auto& p : rec)
+    WIN32_FIND_DATAA file;
+    vector<string>   directories;
+
+    directories.emplace_back(m_path.string());
+    string tmp, tmp1;
+    while (directories.size())
     {
-        extension = p.path().extension().string();
-        //if (extension == ".cpp")
-		if (extension == ".swg")
+        tmp = move(directories.back());
+        directories.pop_back();
+
+		tmp1 = tmp +"\\*";
+        HANDLE h = ::FindFirstFileA(tmp1.c_str(), &file);
+        if (h == INVALID_HANDLE_VALUE)
+            continue;
+
+        do
         {
-            // File filtering by name
-            if (!g_CommandLine.fileFilter.empty() && p.path().string().find(g_CommandLine.fileFilter) == string::npos)
-                continue;
+            tmp1 = tmp + "\\" + file.cFileName;
+            if (file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+            {
+                if ((file.cFileName[0] == '.') && (!file.cFileName[1] || (file.cFileName[1] == '.' && !file.cFileName[2])))
+                    continue;
+                directories.emplace_back(move(tmp1));
+            }
+            else
+            {
+				auto pz = strrchr(file.cFileName, '.');
+                if (pz && !_strcmpi(pz, ".swg"))
+                {
+                    // File filtering by name
+					if (g_CommandLine.fileFilter.empty() || strstr(tmp1.c_str(), g_CommandLine.fileFilter.c_str()))
+					{
+						auto job = g_Pool.m_readFileJob.alloc();
+						auto file = g_Pool.m_sourceFile.alloc();
 
-            auto job  = g_Pool.m_readFileJob.alloc();
-            auto file = g_Pool.m_sourceFile.alloc();
+						job->setFile(file);
+						file->m_path = move(tmp1);
 
-            job->setFile(file);
-            file->m_path = p.path();
+						g_ThreadMgr.addJob(job);
+					}
+                }
+            }
+        } while (::FindNextFileA(h, &file));
 
-            g_ThreadMgr.addJob(job);
-        }
+        ::FindClose(h);
     }
 
-    return true;
+	return true;
 }
