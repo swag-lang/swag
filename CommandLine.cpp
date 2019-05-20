52ff4cde529afbd2d5b63e8949a0d4ded51c755c
cb <christophe.beaudet@gmail.com>
2019-05-20 19:36:37 +0200

    h

--- CommandLine.cpp
+++ CommandLine.cpp
@@ -1,10 +1,13 @@
 #include "pch.h"
 #include "CommandLine.h"
+#include "Global.h"
 
-bool CommandLine::process(int argc, const char *[])
+bool CommandLine::process(int argc, const char *argv[])
 {
 	for (int i = 0; i < argc; i++)
 	{
+		if (!strcmp(argv[i], "--verbose"))
+			g_CommandLine.verbose = true;
 	}
 
 	return true;
