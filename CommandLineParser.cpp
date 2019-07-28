f0c7f0561bdf2e0f378ab6ebdd6b99e50572aebd
cb <christophe.beaudet@gmail.com>
2019-07-28 20:28:37 +0200

    j

--- CommandLineParser.cpp
+++ CommandLineParser.cpp
@@ -28,7 +28,7 @@ void CommandLineParser::setup(CommandLine* cmdLine)
     //cmdLine->runBackendTests = false;
     //cmdLine->addRuntimeModule = false;
     //cmdLine->optimizeByteCode = false;
-	cmdLine->fileFilter = "504";
+	cmdLine->fileFilter = "512";
 }
 
 void CommandLineParser::logArguments()
