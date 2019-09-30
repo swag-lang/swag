955328a116affa34dbb1d282fe941fc4b49fae46
cb <christophe.beaudet@gmail.com>
2019-09-30 06:05:53 +0200

    k

--- Assert.cpp
+++ Assert.cpp
@@ -13,13 +13,13 @@ void swag_assert(const char* expr, const char* file, int line)
 
     g_Log.setColor(LogColor::Red);
     g_Log.print(format("assertion failed: %s:%d: %s\n", file, line, expr));
-	g_Log.setColor(LogColor::Cyan);
+    g_Log.setColor(LogColor::Cyan);
     if (!g_diagnosticInfos.pass.empty())
         g_Log.print(format("pass:        %s\n", g_diagnosticInfos.pass.c_str()));
     if (g_diagnosticInfos.sourceFile)
         g_Log.print(format("source file: %s\n", g_diagnosticInfos.sourceFile->path.string().c_str()));
     if (g_diagnosticInfos.node)
-        g_Log.print(format("source line: %d\n", g_diagnosticInfos.node->token.startLocation.line));
+        g_Log.print(format("source line: %d\n", g_diagnosticInfos.node->token.startLocation.line + 1));
     g_Log.setDefaultColor();
 
     assert(false);
