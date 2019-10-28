65a1cdfaff309cc401d949f369baf7aa2041eb68
cb <christophe.beaudet@gmail.com>
2019-10-28 14:40:36 +0100

    trigo

--- CommandLineParser.cpp
+++ CommandLineParser.cpp
@@ -36,7 +36,7 @@ void CommandLineParser::setup(CommandLine* cmdLine)
     cmdLine->cleanTarget   = false;
     cmdLine->workspacePath = "f:/swag/std";
     //cmdLine->addRuntimeModule = false;
-	cmdLine->rebuild = true;
+	//cmdLine->rebuild = true;
     cmdLine->backendOutput     = true;
     cmdLine->backendOutputTest = true;
     //cmdLine->runBackendTests   = false;
