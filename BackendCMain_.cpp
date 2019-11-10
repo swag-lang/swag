420c71e67cf643da1ed5260d864d9afb1f63627e
cb <christophe.beaudet@gmail.com>
2019-11-10 10:14:37 +0100

    optim

--- BackendCMain_.cpp
+++ BackendCMain_.cpp
@@ -10,56 +10,56 @@
 
 void BackendC::emitArgcArgv()
 {
-    bufferC.addString("static void convertArgcArgv(int argc, char *argv[])\n");
-    bufferC.addString("{\n");
+    CONCAT_FIXED_STR(bufferC, "static void convertArgcArgv(int argc, char *argv[])\n");
+    CONCAT_FIXED_STR(bufferC, "{\n");
 
     bufferC.addStringFormat("\tstatic swag_uint64_t argumentsStr[%d];\n", module->buildParameters.target.backendC.maxApplicationArguments);
     bufferC.addStringFormat("\t__assert(argc <= %d, __FILE__, __LINE__, \"too many application arguments\");\n", module->buildParameters.target.backendC.maxApplicationArguments);
     bufferC.addEol();
 
-    bufferC.addString("\tfor(int i = 0; i < argc; i++) {\n");
-    bufferC.addString("\t\targumentsStr[i * 2] = (swag_int64_t) argv[i];\n");
-    bufferC.addString("\t\targumentsStr[(i * 2) + 1] = (swag_int64_t) strlen(argv[i]);\n");
-    bufferC.addString("\t}\n");
+    CONCAT_FIXED_STR(bufferC, "\tfor(int i = 0; i < argc; i++) {\n");
+    CONCAT_FIXED_STR(bufferC, "\t\targumentsStr[i * 2] = (swag_int64_t) argv[i];\n");
+    CONCAT_FIXED_STR(bufferC, "\t\targumentsStr[(i * 2) + 1] = (swag_int64_t) strlen(argv[i]);\n");
+    CONCAT_FIXED_STR(bufferC, "\t}\n");
     bufferC.addEol();
-    bufferC.addString("\t__process_infos.argumentsSlice[0] = (swag_uint64_t) &argumentsStr[0];\n");
-    bufferC.addString("\t__process_infos.argumentsSlice[1] = (swag_uint64_t) argc;\n");
+    CONCAT_FIXED_STR(bufferC, "\t__process_infos.argumentsSlice[0] = (swag_uint64_t) &argumentsStr[0];\n");
+    CONCAT_FIXED_STR(bufferC, "\t__process_infos.argumentsSlice[1] = (swag_uint64_t) argc;\n");
 
-    bufferC.addString("}\n\n");
+    CONCAT_FIXED_STR(bufferC, "}\n\n");
 }
 
 bool BackendC::emitMain()
 {
     emitSeparator(bufferC, "MAIN");
 
-    bufferC.addString("#ifdef SWAG_IS_BINARY\n");
+    CONCAT_FIXED_STR(bufferC, "#ifdef SWAG_IS_BINARY\n");
     emitArgcArgv();
-    bufferC.addString("void main(int argc, char *argv[])\n");
-    bufferC.addString("{\n");
+    CONCAT_FIXED_STR(bufferC, "void main(int argc, char *argv[])\n");
+    CONCAT_FIXED_STR(bufferC, "{\n");
 
     // Main context
-    bufferC.addString("\tstatic swag_context_t mainContext;\n");
+    CONCAT_FIXED_STR(bufferC, "\tstatic swag_context_t mainContext;\n");
     SWAG_ASSERT(g_defaultContext.allocator);
-    bufferC.addString(format("\tmainContext.allocator = &%s;\n", g_defaultContext.allocator->callName().c_str()));
-    bufferC.addString("\t__process_infos.contextTlsId = __tlsAlloc();\n");
-    bufferC.addString("\t__process_infos.defaultContext = &mainContext;\n");
-    bufferC.addString("\t__tlsSetValue(__process_infos.contextTlsId, __process_infos.defaultContext);\n");
+    bufferC.addStringFormat("\tmainContext.allocator = &%s;\n", g_defaultContext.allocator->callName().c_str());
+    CONCAT_FIXED_STR(bufferC, "\t__process_infos.contextTlsId = __tlsAlloc();\n");
+    CONCAT_FIXED_STR(bufferC, "\t__process_infos.defaultContext = &mainContext;\n");
+    CONCAT_FIXED_STR(bufferC, "\t__tlsSetValue(__process_infos.contextTlsId, __process_infos.defaultContext);\n");
 
     // Arguments
     bufferC.addEol();
-    bufferC.addString("\tconvertArgcArgv(argc, argv);\n");
+    CONCAT_FIXED_STR(bufferC, "\tconvertArgcArgv(argc, argv);\n");
     bufferC.addEol();
 
     // Call to global init of this module, and dependencies
-    bufferC.addString(format("\t%s_globalInit(&__process_infos);\n", module->nameDown.c_str()));
+    bufferC.addStringFormat("\t%s_globalInit(&__process_infos);\n", module->nameDown.c_str());
     for (auto& k : module->moduleDependenciesNames)
     {
         auto nameDown = k;
         replaceAll(nameDown, '.', '_');
         auto depModule = g_Workspace.getModuleByName(k);
         if (depModule->buildParameters.type == BackendOutputType::DynamicLib)
-            bufferC.addString(format("\t__loadDynamicLibrary(\"%s\");\n", nameDown.c_str()));
-        bufferC.addString(format("\t%s_globalInit(&__process_infos);\n", nameDown.c_str()));
+            bufferC.addStringFormat("\t__loadDynamicLibrary(\"%s\");\n", nameDown.c_str());
+        bufferC.addStringFormat("\t%s_globalInit(&__process_infos);\n", nameDown.c_str());
     }
 
     bufferC.addEol();
@@ -67,36 +67,36 @@ bool BackendC::emitMain()
     // Generate call to test functions
     if (!module->byteCodeTestFunc.empty())
     {
-        bufferC.addString("#ifdef SWAG_HAS_TEST\n");
+        CONCAT_FIXED_STR(bufferC, "#ifdef SWAG_HAS_TEST\n");
         for (auto bc : module->byteCodeTestFunc)
         {
             auto node = bc->node;
             if (node && node->attributeFlags & ATTRIBUTE_COMPILER)
                 continue;
-            bufferC.addString(format("\t%s();\n", bc->callName().c_str()));
+            bufferC.addStringFormat("\t%s();\n", bc->callName().c_str());
         }
-        bufferC.addString("#endif\n");
+        CONCAT_FIXED_STR(bufferC, "#endif\n");
         bufferC.addEol();
     }
 
     // Call to main
     if (module->byteCodeMainFunc)
     {
-        bufferC.addString(format("\t%s();\n", module->byteCodeMainFunc->callName().c_str()));
+        bufferC.addStringFormat("\t%s();\n", module->byteCodeMainFunc->callName().c_str());
         bufferC.addEol();
     }
 
     // Call to global drop of this module, and dependencies
-    bufferC.addString(format("\t%s_globalDrop();\n", module->nameDown.c_str()));
+    bufferC.addStringFormat("\t%s_globalDrop();\n", module->nameDown.c_str());
     for (auto& k : module->moduleDependenciesNames)
     {
         auto nameDown = k;
         replaceAll(nameDown, '.', '_');
-        bufferC.addString(format("\t%s_globalDrop();\n", nameDown.c_str()));
+        bufferC.addStringFormat("\t%s_globalDrop();\n", nameDown.c_str());
     }
 
-    bufferC.addString("}\n");
-    bufferC.addString("#endif /* SWAG_IS_BINARY */\n");
+    CONCAT_FIXED_STR(bufferC, "}\n");
+    CONCAT_FIXED_STR(bufferC, "#endif /* SWAG_IS_BINARY */\n");
     bufferC.addEol();
 
     return true;
