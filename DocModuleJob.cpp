40bc1e14ceb0198f8b7e43ced7f0648e9a2e5e28
cb <christophe.beaudet@gmail.com>
2019-12-10 13:47:43 +0100

    copy css

--- DocModuleJob.cpp
+++ DocModuleJob.cpp
@@ -3,11 +3,13 @@
 #include "DocScopeJob.h"
 #include "Workspace.h"
 #include "ThreadManager.h"
+#include "CopyFileJob.h"
 
 thread_local Pool<DocModuleJob> g_Pool_docModuleJob;
 
 JobResult DocModuleJob::execute()
 {
+    // Document path for the module
     module->documentPath = g_Workspace.workspacePath;
     module->documentPath.append("documentation/");
     module->documentPath.append(module->name);
@@ -20,6 +22,22 @@ JobResult DocModuleJob::execute()
         return JobResult::ReleaseJob;
     }
 
+    // Clear content if rebuild
+    if (g_CommandLine.rebuild)
+    {
+        g_Workspace.deleteFolderContent(module->documentPath);
+    }
+
+    // Copy css to documentation folder
+    fs::path p          = g_CommandLine.exePath;
+    auto     path       = p.parent_path().string() + "/swag.documentation.css";
+    auto     copyJob    = g_Pool_copyFileJob.alloc();
+    copyJob->module     = module;
+    copyJob->sourcePath = path;
+    copyJob->destPath   = module->documentPath.string() + "/swag.documentation.css";
+    g_ThreadMgr.addJob(copyJob);
+
+    // Scan module root scope
     auto scopeJob    = g_Pool_docScopeJob.alloc();
     scopeJob->module = module;
     scopeJob->scope  = module->scopeRoot;
