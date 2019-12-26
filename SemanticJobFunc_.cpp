457cc80150d0ae64f339bfa78c0420b96a271cb4
cb <christophe.beaudet@gmail.com>
2019-12-26 09:12:39 +0100

    verify that code type is valid

--- SemanticJobFunc_.cpp
+++ SemanticJobFunc_.cpp
@@ -38,6 +38,12 @@ bool SemanticJob::setupFuncDeclParams(SemanticContext* context, TypeInfoFuncAttr
         funcParam->index      = index++;
         funcParam->node       = nodeParam;
 
+        // Code is only valid for a macro or mixin
+        if (nodeParam->typeInfo->kind == TypeInfoKind::Code)
+        {
+            SWAG_VERIFY(funcAttr->attributeFlags & (ATTRIBUTE_MACRO | ATTRIBUTE_MIXIN), context->report({nodeParam, "type 'code' is only valid in a 'swag.macro' or 'swag.inline' function"}));
+        }
+
         // Not everything is possible for types for attributes
         if (param->ownerScope->kind == ScopeKind::Attribute)
         {
