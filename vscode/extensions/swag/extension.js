f6ac49af11b798bf9766d495dc26ba5de5cb8b4b
cb <christophe.beaudet@gmail.com>
2020-01-05 12:20:34 +0100

    vscode

--- vscode/extensions/swag/extension.js
+++ vscode/extensions/swag/extension.js
@@ -1,10 +1,13 @@
 const vscode = require('vscode');
-const taskProvider = require('./src/taskprovider');
+const os = require('os');
+const providers = require('./src/providers');
 
 function activate(context) 
 {
-	vscode.tasks.registerTaskProvider("swag-build", new taskProvider.SwagTaskProvider);
-	taskProvider.launchBackgroundTasks();
+    vscode.tasks.registerTaskProvider("swag-build", new providers.TaskProvider);
+    context.subscriptions.push(vscode.languages.registerDefinitionProvider("swag", new providers.GoToDefinitionProvider()));
+
+    providers.launchBackgroundTasks();  
 }
 
 exports.activate = activate;
