56c0b9165088e76db89777d0800da348b773d66c
cb <christophe.beaudet@gmail.com>
2020-07-09 17:08:08 +0200

    var multi declarations

--- SyntaxJobVar_.cpp
+++ SyntaxJobVar_.cpp
@@ -29,48 +29,54 @@ bool SyntaxJob::doVarDecl(AstNode* parent, AstNode** result)
 bool SyntaxJob::doVarDecl(AstNode* parent, AstNode** result, AstNodeKind kind)
 {
     AstNode* leftNode;
-    SWAG_CHECK(doLeftExpression(&leftNode));
-
-    SWAG_VERIFY(token.id != TokenId::SymEqualEqual, syntaxError(token, "invalid compare operator '==', do you mean '=' ?"));
-    SWAG_VERIFY(token.id == TokenId::SymColon || token.id == TokenId::SymEqual, syntaxError(token, format("invalid token '%s' in variable declaration, ':' or '=' are expected here", token.text.c_str())));
-
-    AstNode* type = nullptr;
-    if (token.id == TokenId::SymColon)
+    while (true)
     {
-        SWAG_CHECK(eatToken());
-        SWAG_CHECK(doTypeExpression(nullptr, &type, true));
-    }
+        SWAG_CHECK(doLeftExpression(&leftNode));
 
-    AstNode* assign = nullptr;
-    SWAG_VERIFY(token.id != TokenId::SymEqualEqual, syntaxError(token, "invalid compare operator '==', do you mean '=' ?"));
-    if (token.id == TokenId::SymEqual)
-    {
+        SWAG_VERIFY(token.id != TokenId::SymEqualEqual, syntaxError(token, "invalid compare operator '==', do you mean '=' ?"));
+        SWAG_VERIFY(token.id == TokenId::SymColon || token.id == TokenId::SymEqual, syntaxError(token, format("invalid token '%s' in variable declaration, ':' or '=' are expected here", token.text.c_str())));
+
+        AstNode* type = nullptr;
+        if (token.id == TokenId::SymColon)
+        {
+            SWAG_CHECK(eatToken());
+            SWAG_CHECK(doTypeExpression(nullptr, &type, true));
+        }
+
+        AstNode* assign = nullptr;
+        SWAG_VERIFY(token.id != TokenId::SymEqualEqual, syntaxError(token, "invalid compare operator '==', do you mean '=' ?"));
+        if (token.id == TokenId::SymEqual)
+        {
+            SWAG_CHECK(eatToken());
+            SWAG_CHECK(doInitializationExpression(nullptr, &assign));
+        }
+
+        // Be sure we will be able to have a type
+        if (!type && !assign)
+            return error(leftNode->token, "variable must have a type or must be initialized");
+
+        SWAG_CHECK(doVarDeclExpression(parent, leftNode, type, assign, kind, result));
+
+        // If we have a type, and that type has parameters (struct construction), then we need to evaluate and push the parameters
+        if (type && type->kind == AstNodeKind::TypeExpression)
+        {
+            auto typeExpression = CastAst<AstTypeExpression>(type, AstNodeKind::TypeExpression);
+            if (typeExpression->identifier && typeExpression->identifier->kind == AstNodeKind::IdentifierRef)
+            {
+                auto identifier = CastAst<AstIdentifier>(typeExpression->identifier->childs.back(), AstNodeKind::Identifier);
+                if (identifier->callParameters)
+                {
+                    typeExpression->flags &= ~AST_NO_BYTECODE_CHILDS;
+                    typeExpression->flags |= AST_HAS_STRUCT_PARAMETERS;
+                }
+            }
+        }
+
+        if (token.id != TokenId::SymComma)
+            break;
         SWAG_CHECK(eatToken());
-        SWAG_CHECK(doInitializationExpression(nullptr, &assign));
     }
 
     SWAG_CHECK(eatSemiCol("at the end of a variable declation"));
-
-    // Be sure we will be able to have a type
-    if (!type && !assign)
-        return error(leftNode->token, "variable must have a type or must be initialized");
-
-    SWAG_CHECK(doVarDeclExpression(parent, leftNode, type, assign, kind, result));
-
-    // If we have a type, and that type has parameters (struct construction), then we need to evaluate and push the parameters
-    if (type && type->kind == AstNodeKind::TypeExpression)
-    {
-        auto typeExpression = CastAst<AstTypeExpression>(type, AstNodeKind::TypeExpression);
-        if (typeExpression->identifier && typeExpression->identifier->kind == AstNodeKind::IdentifierRef)
-        {
-            auto identifier = CastAst<AstIdentifier>(typeExpression->identifier->childs.back(), AstNodeKind::Identifier);
-            if (identifier->callParameters)
-            {
-                typeExpression->flags &= ~AST_NO_BYTECODE_CHILDS;
-                typeExpression->flags |= AST_HAS_STRUCT_PARAMETERS;
-            }
-        }
-    }
-
     return true;
 }
