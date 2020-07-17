d688c5c91bab6e48f40748576b843eb241e78634
cb <christophe.beaudet@gmail.com>
2020-07-17 14:15:07 +0200

    refac

--- BackendLinkerWin32.h
+++ BackendLinkerWin32.h
@@ -1,6 +1,14 @@
 #pragma once
+#ifdef _WIN32
+struct Module;
+struct BuildParameters;
 
 namespace BackendLinkerWin32
 {
     void getLibPaths(vector<Utf8>& libPath);
-}
+
+    string getResultFile(const BuildParameters& buildParameters);
+    void   getArguments(const BuildParameters& buildParameters, Module* module, Utf8& arguments);
+} // namespace BackendLinkerWin32
+
+#endif
