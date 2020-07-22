2495e73868bd5a1b30fc26f931ce85fcfc7ece44
cb <christophe.beaudet@gmail.com>
2020-07-22 18:04:02 +0200

    remove multi inits

--- ByteCodeGenJobExpression_.cpp
+++ ByteCodeGenJobExpression_.cpp
@@ -203,6 +203,7 @@ bool ByteCodeGenJob::emitLiteral(ByteCodeGenContext* context, AstNode* node, Typ
             reserveLinearRegisterRC(context, regList, 2);
             emitInstruction(context, ByteCodeOp::ClearRA, regList[0]);
             emitInstruction(context, ByteCodeOp::ClearRA, regList[1]);
+            node->castedTypeInfo = nullptr;
             return true;
         }
 
@@ -213,6 +214,7 @@ bool ByteCodeGenJob::emitLiteral(ByteCodeGenContext* context, AstNode* node, Typ
             emitInstruction(context, ByteCodeOp::ClearRA, regList[1]);
             emitInstruction(context, ByteCodeOp::ClearRA, regList[2]);
             emitInstruction(context, ByteCodeOp::CopyRBAddrToRA, regList[0], regList[1]);
+            node->castedTypeInfo = nullptr;
             return true;
         }
     }
