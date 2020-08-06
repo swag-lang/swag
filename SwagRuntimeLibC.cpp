ab88ec17fdbe7c6e2338b8911e4951db3c5ac514
cb <christophe.beaudet@gmail.com>
2020-08-06 10:34:02 +0200

    crt

--- SwagRuntimeLibC.cpp
+++ SwagRuntimeLibC.cpp
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include "SwagRuntimeLibC.h"
 
+// Only contains function that are not part of ucrt.lib, as for now we use the windows crt and
+// only compile for windows
+
 /////////////////////////////////////////////////////////////////////////////////////////////
 EXTERN_C void* memcpy(void* dst, const void* src, size_t size)
 {
@@ -26,64 +29,3 @@ EXTERN_C int memcmp(const void* b1, const void* b2, size_t n)
     }
     return 0;
 }
-
-#if 0
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-EXTERN_C size_t strlen(const char* src)
-{
-    const char* end = src;
-    while (*end != '\0')
-        ++end;
-    return end - src;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-EXTERN_C void* memset(void* dst, int val, size_t size)
-{
-    char* realdst = (char*) dst;
-    for (size_t i = 0; i < size; i++)
-        realdst[i] = (char) val;
-    return dst;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-EXTERN_C void exit(int code)
-{
-#ifdef _WIN32
-    extern void ExitProcess(int);
-    ExitProcess(code);
-#endif
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-EXTERN_C void* malloc(size_t len)
-{
-#ifdef _WIN32
-    void* HeapAlloc(void*, int, size_t);
-    void* GetProcessHeap();
-    return HeapAlloc(GetProcessHeap(), 0, len);
-#endif
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-EXTERN_C void free(void* src)
-{
-#ifdef _WIN32
-    int   HeapFree(void*, int, void*);
-    void* GetProcessHeap();
-    HeapFree(GetProcessHeap(), 0, src);
-#endif
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-EXTERN_C void* realloc(void* src, size_t newSize)
-{
-#ifdef _WIN32
-    void* HeapReAlloc(void*, int, void*, size_t);
-    void* GetProcessHeap();
-    return HeapReAlloc(GetProcessHeap(), 0, src, newSize);
-#endif
-}
-
-#endif
