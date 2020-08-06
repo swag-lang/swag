703e14abbb8531cf3c29664974128e2330d8eecb
cb <christophe.beaudet@gmail.com>
2020-08-06 10:56:33 +0200

    crt

--- libc/win32/crt.cpp
+++ libc/win32/crt.cpp
@@ -2,21 +2,23 @@
 #include <stdint.h>
 #include "../libc.h"
 
-EXTERN_C int _fltused = 0;
-
-EXTERN_C void mainCRTStartup()
+EXTERN_C
 {
-    const char* GetCommandLineA();
-    __main(GetCommandLineA());
-}
+    int _fltused = 0;
 
-EXTERN_C int _DllMainCRTStartup(void*, int, void*)
-{
-    return 1;
-}
+    void __chkstk()
+    {
+    }
 
-extern "C" void __chkstk()
-{
+    void mainCRTStartup()
+    {
+        __main(GetCommandLineA());
+    }
+
+    int _DllMainCRTStartup(void*, int, void*)
+    {
+        return 1;
+    }
 }
 
 #endif // _WIN32
--- libc/win32/win32.h
+++ libc/win32/win32.h
@@ -2,10 +2,11 @@
 
 #define STD_OUTPUT_HANDLE -11
 
-EXTERN_C void* GetStdHandle(int);
-EXTERN_C int   WriteFile(void*, const void*, int, const int*, void*);
-EXTERN_C void  RaiseException(int, int, int, const void*);
-EXTERN_C void* LoadLibraryA(const char*);
-EXTERN_C int   TlsAlloc();
-EXTERN_C void  TlsSetValue(int, void*);
-EXTERN_C void* TlsGetValue(int);
+EXTERN_C void*       GetStdHandle(int);
+EXTERN_C int         WriteFile(void*, const void*, int, const int*, void*);
+EXTERN_C void        RaiseException(int, int, int, const void*);
+EXTERN_C void*       LoadLibraryA(const char*);
+EXTERN_C int         TlsAlloc();
+EXTERN_C void        TlsSetValue(int, void*);
+EXTERN_C void*       TlsGetValue(int);
+EXTERN_C const char* GetCommandLineA();
