1412fef13ea559304449dd6068c4570f32789810
cb <christophe.beaudet@gmail.com>
2020-08-08 10:37:26 +0200

    getbuildcfg !

--- CompilerItf.cpp
+++ CompilerItf.cpp
@@ -6,11 +6,17 @@ void* getMessage(Module* module)
     return (void*) module->currentCompilerMessage;
 }
 
+void* getBuildCfg(Module* module)
+{
+    return (void*) &module->buildCfg;
+}
+
 using cb = void* (*) (Module*);
 
 static cb itable[] =
     {
         getMessage,
+        getBuildCfg,
 };
 
 static void* iitf[] = {nullptr, (void*) itable};
