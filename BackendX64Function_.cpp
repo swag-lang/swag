e23f4d38a65f241ae17b12d0ec119f988ed9d106
cb <christophe.beaudet@gmail.com>
2020-08-12 18:27:03 +0200

    x64

--- BackendX64Function_.cpp
+++ BackendX64Function_.cpp
@@ -624,6 +624,12 @@ bool BackendX64::emitFunctionBody(const BuildParameters& buildParameters, Module
             concat.addU32(0);
             break;
 
+        case ByteCodeOp::IncrementRA32:
+            //CONCAT_STR_1(concat, "r[", ip->a.u32, "].u32++;");
+            BackendX64Inst::emit_Move_Reg_In_RAX(pp, ip->a.u32);
+            BackendX64Inst::emit_Add_Cst32_To_RAX(pp, 1);
+            BackendX64Inst::emit_Move_RAX_At_Reg(pp, ip->a.u32);
+            break;
         case ByteCodeOp::DecrementRA32:
             //CONCAT_STR_1(concat, "r[", ip->a.u32, "].u32--;");
             BackendX64Inst::emit_Move_Reg_In_RAX(pp, ip->a.u32);
