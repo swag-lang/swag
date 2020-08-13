4dd7d5de799027aedc190d003e5485275eb044b2
cb <christophe.beaudet@gmail.com>
2020-08-13 10:02:19 +0200

    x64

--- BackendX64Function_.cpp
+++ BackendX64Function_.cpp
@@ -156,6 +156,15 @@ bool BackendX64::emitFunctionBody(const BuildParameters& buildParameters, Module
             BackendX64Inst::emit_Move_XMM0_At_Reg_F64(pp, ip->a.u32);
             break;
 
+        case ByteCodeOp::BinOpXorU32:
+            //concat.addStringFormat("r[%u].s32 = r[%u].s32 ^ r[%u].s32;", ip->c.u32, ip->a.u32, ip->b.u32);
+            BackendX64Inst::emit_BinOpInt_At_Reg(pp, ip, 0x33, 32);
+            break;
+        case ByteCodeOp::BinOpXorU64:
+            //concat.addStringFormat("r[%u].s32 = r[%u].s32 ^ r[%u].s32;", ip->c.u32, ip->a.u32, ip->b.u32);
+            BackendX64Inst::emit_BinOpInt_At_Reg(pp, ip, 0x33, 64);
+            break;
+
         case ByteCodeOp::BinOpMulS32:
             //concat.addStringFormat("r[%u].s32 = r[%u].s32 * r[%u].s32;", ip->c.u32, ip->a.u32, ip->b.u32);
             BackendX64Inst::emit_BinOpInt_At_Reg(pp, ip, 0xAF0F, 32);
@@ -171,6 +180,11 @@ bool BackendX64::emitFunctionBody(const BuildParameters& buildParameters, Module
         case ByteCodeOp::BinOpMulF64:
             //concat.addStringFormat("r[%u].f64 = r[%u].f64 * r[%u].f64;", ip->c.u32, ip->a.u32, ip->b.u32);
             BackendX64Inst::emit_BinOpFloat_At_Reg(pp, ip, 0x59, 64);
+            break;
+
+        case ByteCodeOp::BinOpDivS32:
+            //concat.addStringFormat("r[%u].s32 = r[%u].s32 / r[%u].s32;", ip->c.u32, ip->a.u32, ip->b.u32);
+            BackendX64Inst::emit_BinOpInt_At_Reg(pp, ip, 0xAF0F, 32);
             break;
 
         case ByteCodeOp::BinOpPlusS32:
