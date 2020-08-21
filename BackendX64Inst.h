6dc125e3f9f1e657e709432684806a16f08832b5
cb <christophe.beaudet@gmail.com>
2020-08-21 17:46:33 +0200

    x64

--- BackendX64Inst.h
+++ BackendX64Inst.h
@@ -346,11 +346,18 @@ namespace BackendX64Inst
         }
     }
 
+    inline void emit_Copy64(X64PerThread& pp, uint32_t regDst, uint32_t regSrc)
+    {
+        pp.concat.addU8(0x48 | ((regDst & 0b1000) >> 3) | ((regSrc & 0b1000) >> 1));
+        pp.concat.addU8(0x89);
+        pp.concat.addU8(modRM(0b11, regSrc & 0b111, regDst & 0b111));
+    }
+
     inline void emit_Lea_Stack_In_RAX(X64PerThread& pp, uint32_t stackOffset)
     {
         if (stackOffset == 0)
         {
-            pp.concat.addString3("\x48\x89\xF8"); // mov rax, rdi
+            emit_Copy64(pp, RAX, RDI);
         }
         else if (stackOffset <= 0x7F)
         {
