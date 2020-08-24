0bf3064d5e4d1fb8bc2fdbb952a2e39d3674019e
cb <christophe.beaudet@gmail.com>
2020-08-24 16:31:36 +0200

    remove seek from source location

--- SourceLocation.h
+++ SourceLocation.h
@@ -1,11 +1,7 @@
 #pragma once
 
-// Number of lines of code printed for an error
-static const int REPORT_NUM_CODE_LINES = 3;
-
 struct SourceLocation
 {
     int line;
     int column;
-    int seekStartLine[REPORT_NUM_CODE_LINES];
 };
