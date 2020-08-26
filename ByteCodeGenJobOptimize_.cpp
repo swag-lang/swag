960bfe4e8280a1e30f5eef923ad6273d2c088cda
cb <christophe.beaudet@gmail.com>
2020-08-26 22:11:27 +0200

    jump optims

--- ByteCodeGenJobOptimize_.cpp
+++ ByteCodeGenJobOptimize_.cpp
@@ -16,7 +16,13 @@ static bool optimizeJumps(ByteCode* bc)
     auto ip               = bc->out;
     while (ip->op != ByteCodeOp::End)
     {
-        if (ip->op == ByteCodeOp::Jump)
+        if (ip->op == ByteCodeOp::Jump ||
+            ip->op == ByteCodeOp::JumpIfTrue ||
+            ip->op == ByteCodeOp::JumpIfFalse ||
+            ip->op == ByteCodeOp::JumpIfZero32 ||
+            ip->op == ByteCodeOp::JumpIfZero64 ||
+            ip->op == ByteCodeOp::JumpIfNotZero32 ||
+            ip->op == ByteCodeOp::JumpIfNotZero64)
         {
             // Jump to another jump
             auto destIp = ip + ip->b.s32 + 1;
@@ -29,6 +35,24 @@ static bool optimizeJumps(ByteCode* bc)
 
             // Jump to the next instruction
             if (ip->b.s32 == 0)
+            {
+                hasDoneSomething = true;
+                ip->op           = ByteCodeOp::Nop;
+            }
+
+            // Next instruction is a nop, move the jump forward if the jump is positive
+            else if (ip[1].op == ByteCodeOp::Nop && ip->b.s32 > 0)
+            {
+                hasDoneSomething = true;
+                std::swap(ip[0], ip[1]);
+                ip[1].b.s32--;
+            }
+        }
+
+        if (ip->op == ByteCodeOp::Jump)
+        {
+            // Next instruction is a jump to the same target
+            if (ip[1].op == ByteCodeOp::Jump && (ip->b.s32 - 1 == ip[1].b.s32))
             {
                 hasDoneSomething = true;
                 ip->op           = ByteCodeOp::Nop;
@@ -51,5 +75,5 @@ void ByteCodeGenJob::optimize(ByteCodeGenContext* context)
     //if (module->mustOptimizeBC(job->originalNode) < 2)
     //    return;
 
-    optimizeJumps(context->bc);
+    while (optimizeJumps(context->bc)) {}
 }
