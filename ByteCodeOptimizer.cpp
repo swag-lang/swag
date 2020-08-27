28ac222126d75ca11cb5c2bda30e3722859405c8
cb <christophe.beaudet@gmail.com>
2020-08-27 08:53:21 +0200

    bc opt

--- ByteCodeOptimizer.cpp
+++ ByteCodeOptimizer.cpp
@@ -6,12 +6,6 @@
 #include "ByteCodeOp.h"
 #include "Module.h"
 
-inline void setNop(ByteCodeOptContext* context, ByteCodeInstruction* ip)
-{
-    ip->op = ByteCodeOp::Nop;
-    context->nops.push_back(ip);
-}
-
 bool ByteCodeOptimizer::optimizeJumps(ByteCodeOptContext* context)
 {
     bool hasDoneSomething = false;
@@ -61,20 +55,54 @@ bool ByteCodeOptimizer::optimizeJumps(ByteCodeOptContext* context)
     return hasDoneSomething;
 }
 
-void ByteCodeOptimizer::optimize(ByteCodeGenContext* context)
+bool ByteCodeOptimizer::removeNops(ByteCodeOptContext* context)
 {
-    if (!context->bc)
-        return;
+    bool hasDoneSomething = false;
 
-    auto job    = context->job;
-    auto module = job->originalNode->sourceFile->module;
-    //if (module->mustOptimizeBC(job->originalNode) < 2)
-    //    return;
+    // Adapt all jumps
+    for (int idx = 0; idx < context->jumps.size(); idx++)
+    {
+        auto ip      = context->jumps[idx];
+        auto srcJump = (int) (ip - context->bc->out);
+        auto dstJump = srcJump + ip->b.s32 + 1;
 
-    ByteCodeOptContext optContext;
-    optContext.bc = context->bc;
+        for (auto nop : context->nops)
+        {
+            auto idxNop = (int) (nop - context->bc->out);
+            if (srcJump < dstJump && idxNop > srcJump && idxNop <= dstJump)
+            {
+                ip->b.s32--;
+                hasDoneSomething = true;
+            }
+            else if (srcJump > dstJump && idxNop >= dstJump && idxNop < srcJump)
+            {
+                ip->b.s32++;
+                hasDoneSomething = true;
+            }
+        }
+    }
 
-    // Get all jumps
+    if (!hasDoneSomething)
+        return false;
+
+    auto ip     = context->bc->out;
+    auto ipDest = context->bc->out;
+    for (uint32_t i = 0; i < context->bc->numInstructions; i++)
+    {
+        if (ip->op != ByteCodeOp::Nop)
+            *ipDest++ = *ip++;
+        else
+            ip++;
+    }
+
+    context->bc->numInstructions -= (int) context->nops.size();
+    context->nops.clear();
+    return true;
+}
+
+bool ByteCodeOptimizer::setJumps(ByteCodeOptContext* context)
+{
+    context->jumps.clear();
     for (auto ip = context->bc->out; ip->op != ByteCodeOp::End; ip++)
     {
         if (ip->op == ByteCodeOp::Jump ||
@@ -85,9 +113,36 @@ void ByteCodeOptimizer::optimize(ByteCodeGenContext* context)
             ip->op == ByteCodeOp::JumpIfNotZero32 ||
             ip->op == ByteCodeOp::JumpIfNotZero64)
         {
-            optContext.jumps.push_back(ip);
+            context->jumps.push_back(ip);
         }
     }
 
-    while (optimizeJumps(&optContext)) {}
+    return !context->jumps.empty();
+}
+
+void ByteCodeOptimizer::optimize(ByteCodeGenContext* context)
+{
+    if (!context->bc)
+        return;
+
+    auto job    = context->job;
+    auto module = job->originalNode->sourceFile->module;
+    //if (module->mustOptimizeBC(job->originalNode) < 2)
+    //   return;
+    if (job->originalNode->sourceFile->name == "compiler1904.swg")
+        job = job;
+
+    ByteCodeOptContext optContext;
+    optContext.bc = context->bc;
+
+    // Get all jumps
+
+    while (true)
+    {
+        if (!setJumps(&optContext))
+            break;
+        if (!optimizeJumps(&optContext))
+            break;
+        removeNops(&optContext);
+    }
 }
--- ByteCodeOptimizer.h
+++ ByteCodeOptimizer.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "ByteCode.h"
+#include "ByteCodeOp.h"
 struct ByteCode;
 struct ByteCodeGenContext;
 struct ByteCodeInstruction;
@@ -12,6 +14,16 @@ struct ByteCodeOptContext
 
 struct ByteCodeOptimizer
 {
+    inline static void setNop(ByteCodeOptContext* context, ByteCodeInstruction* ip)
+    {
+        ip->op = ByteCodeOp::Nop;
+        context->nops.push_back(ip);
+    }
+
     static bool optimizeJumps(ByteCodeOptContext* context);
+
+    static bool setJumps(ByteCodeOptContext* context);
+    static bool removeNops(ByteCodeOptContext* context);
+
     static void optimize(ByteCodeGenContext* context);
 };
