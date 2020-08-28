f7cb6364c29427aa697a5118d1659c842d1bc347
cb <christophe.beaudet@gmail.com>
2020-08-28 08:44:57 +0200

    bc optim pass immediate, prep for future

--- ByteCodeOptimizer_imm_.cpp
+++ ByteCodeOptimizer_imm_.cpp
@@ -33,12 +33,36 @@ void ByteCodeOptimizer::optimizePassImmediate(ByteCodeOptContext* context)
             regs[ip->a.u32]   = ip;
         }
 
+        if ((flags & OPFLAG_IMM_A) && !(ip->flags & BCI_IMM_A) && (flags & OPFLAG_READ_A) && regs[ip->a.u32])
+        {
+            context->passHasDoneSomething = true;
+            ip->flags |= BCI_IMM_A;
+            regs[ip->a.u32] = nullptr;
+            ip->a.u64       = regsRW[ip->a.u32];
+        }
+
         if ((flags & OPFLAG_IMM_B) && !(ip->flags & BCI_IMM_B) && (flags & OPFLAG_READ_B) && regs[ip->b.u32])
         {
             context->passHasDoneSomething = true;
             ip->flags |= BCI_IMM_B;
             regs[ip->b.u32] = nullptr;
             ip->b.u64       = regsRW[ip->b.u32];
+        }
+
+        if ((flags & OPFLAG_IMM_C) && !(ip->flags & BCI_IMM_C) && (flags & OPFLAG_READ_C) && regs[ip->c.u32])
+        {
+            context->passHasDoneSomething = true;
+            ip->flags |= BCI_IMM_C;
+            regs[ip->c.u32] = nullptr;
+            ip->c.u64       = regsRW[ip->c.u32];
+        }
+
+        if ((flags & OPFLAG_IMM_D) && !(ip->flags & BCI_IMM_D) && (flags & OPFLAG_READ_D) && regs[ip->d.u32])
+        {
+            context->passHasDoneSomething = true;
+            ip->flags |= BCI_IMM_D;
+            regs[ip->d.u32] = nullptr;
+            ip->d.u64       = regsRW[ip->d.u32];
         }
 
         if (flags & OPFLAG_READ_A && !(ip->flags & BCI_IMM_A))
