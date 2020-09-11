03e1edf3b6e8842a28c97a35b598b5b034b3cac7
cb <christophe.beaudet@gmail.com>
2020-09-11 11:27:21 +0200

    compare type, pre check before name

--- swag_runtime_compareType.cpp
+++ swag_runtime_compareType.cpp
@@ -12,5 +12,7 @@ EXTERN_C bool swag_runtime_compareType(const void* type1, const void* type2)
 
     auto ctype1 = (ConcreteTypeInfo*) type1;
     auto ctype2 = (ConcreteTypeInfo*) type2;
+    if ((ctype1->kind != ctype2->kind) || (ctype1->sizeOf != ctype2->sizeOf) || (ctype1->flags != ctype2->flags))
+        return false;
     return swag_runtime_compareString(ctype1->name.buffer, ctype2->name.buffer, (SwagU32) ctype1->name.count, (SwagU32) ctype2->name.count);
 }
