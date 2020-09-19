b750a4756273997777ffab1b5976fc80489841a4
cb <christophe.beaudet@gmail.com>
2020-09-19 20:00:34 +0200

    remove dead lock between typejob and storeToSegment

--- TypeTableJob.cpp
+++ TypeTableJob.cpp
@@ -12,70 +12,90 @@ bool TypeTableJob::computeStruct()
     auto realType     = (TypeInfoStruct*) typeInfo;
     auto segment      = typeTable->getConstantSegment(module, cflags);
 
-    unique_lock lk1(segment->mutex);
-
-    SWAG_CHECK(typeTable->makeConcreteAttributes(baseContext, realType->attributes, &concreteType->attributes, OFFSETOF(concreteType->attributes), cflags));
-
-    // Fields
-    concreteType->fields.buffer = nullptr;
-    concreteType->fields.count  = realType->fields.size();
-    if (concreteType->fields.count)
+    // First and main pass, by locking only the type segment
     {
-        uint32_t               storageArray = segment->reserveNoLock((uint32_t) concreteType->fields.count * sizeof(ConcreteTypeInfoParam));
-        ConcreteTypeInfoParam* addrArray    = (ConcreteTypeInfoParam*) segment->addressNoLock(storageArray);
-        concreteType->fields.buffer         = addrArray;
-        segment->addInitPtr(OFFSETOF(concreteType->fields.buffer), storageArray);
-        for (int param = 0; param < concreteType->fields.count; param++)
-        {
-            SWAG_CHECK(typeTable->makeConcreteParam(baseContext, addrArray + param, storageArray, realType->fields[param], cflags));
-            storageArray += sizeof(ConcreteTypeInfoParam);
-        }
-    }
+        unique_lock lk1(segment->mutex);
 
-    // Methods
-    concreteType->methods.buffer = nullptr;
-    concreteType->methods.count  = realType->methods.size();
-    if (concreteType->methods.count)
-    {
-        uint32_t               storageArray = segment->reserveNoLock((uint32_t) concreteType->methods.count * sizeof(ConcreteTypeInfoParam));
-        ConcreteTypeInfoParam* addrArray    = (ConcreteTypeInfoParam*) segment->addressNoLock(storageArray);
-        concreteType->methods.buffer        = addrArray;
-        segment->addInitPtr(OFFSETOF(concreteType->methods.buffer), storageArray);
-        for (int param = 0; param < concreteType->methods.count; param++)
-        {
-            SWAG_CHECK(typeTable->makeConcreteParam(baseContext, addrArray + param, storageArray, realType->methods[param], cflags));
-            storageArray += sizeof(ConcreteTypeInfoParam);
-        }
-    }
+        SWAG_CHECK(typeTable->makeConcreteAttributes(baseContext, realType->attributes, &concreteType->attributes, OFFSETOF(concreteType->attributes), cflags));
 
-    // Interfaces
-    concreteType->interfaces.buffer = nullptr;
-    concreteType->interfaces.count  = realType->interfaces.size();
-    if (concreteType->interfaces.count)
-    {
-        uint32_t               storageArray = segment->reserveNoLock((uint32_t) concreteType->interfaces.count * sizeof(ConcreteTypeInfoParam));
-        ConcreteTypeInfoParam* addrArray    = (ConcreteTypeInfoParam*) segment->addressNoLock(storageArray);
-        concreteType->interfaces.buffer     = addrArray;
-        segment->addInitPtr(OFFSETOF(concreteType->interfaces.buffer), storageArray);
-        for (int param = 0; param < concreteType->interfaces.count; param++)
+        // Fields
+        concreteType->fields.buffer = nullptr;
+        concreteType->fields.count  = realType->fields.size();
+        if (concreteType->fields.count)
         {
-            SWAG_CHECK(typeTable->makeConcreteParam(baseContext, addrArray + param, storageArray, realType->interfaces[param], cflags));
-
-            // Compute the storage of the interface for swag_runtime_interfaceof
-            // Not needed if we are computing a compiler type
-            if (!(cflags & CONCRETE_FOR_COMPILER))
+            uint32_t               storageArray = segment->reserveNoLock((uint32_t) concreteType->fields.count * sizeof(ConcreteTypeInfoParam));
+            ConcreteTypeInfoParam* addrArray    = (ConcreteTypeInfoParam*) segment->addressNoLock(storageArray);
+            concreteType->fields.buffer         = addrArray;
+            segment->addInitPtr(OFFSETOF(concreteType->fields.buffer), storageArray);
+            for (int param = 0; param < concreteType->fields.count; param++)
             {
-                uint32_t fieldOffset   = offsetof(ConcreteTypeInfoParam, value);
-                uint32_t valueOffset   = storageArray + fieldOffset;
-                addrArray[param].value = module->constantSegment.address(realType->interfaces[param]->offset);
-                segment->addInitPtr(valueOffset, realType->interfaces[param]->offset, SegmentKind::Constant);
+                SWAG_CHECK(typeTable->makeConcreteParam(baseContext, addrArray + param, storageArray, realType->fields[param], cflags));
+                storageArray += sizeof(ConcreteTypeInfoParam);
             }
+        }
 
-            storageArray += sizeof(ConcreteTypeInfoParam);
+        // Methods
+        concreteType->methods.buffer = nullptr;
+        concreteType->methods.count  = realType->methods.size();
+        if (concreteType->methods.count)
+        {
+            uint32_t               storageArray = segment->reserveNoLock((uint32_t) concreteType->methods.count * sizeof(ConcreteTypeInfoParam));
+            ConcreteTypeInfoParam* addrArray    = (ConcreteTypeInfoParam*) segment->addressNoLock(storageArray);
+            concreteType->methods.buffer        = addrArray;
+            segment->addInitPtr(OFFSETOF(concreteType->methods.buffer), storageArray);
+            for (int param = 0; param < concreteType->methods.count; param++)
+            {
+                SWAG_CHECK(typeTable->makeConcreteParam(baseContext, addrArray + param, storageArray, realType->methods[param], cflags));
+                storageArray += sizeof(ConcreteTypeInfoParam);
+            }
+        }
+
+        // Interfaces
+        concreteType->interfaces.buffer = nullptr;
+        concreteType->interfaces.count  = realType->interfaces.size();
+        if (concreteType->interfaces.count)
+        {
+            uint32_t               storageArray = segment->reserveNoLock((uint32_t) concreteType->interfaces.count * sizeof(ConcreteTypeInfoParam));
+            ConcreteTypeInfoParam* addrArray    = (ConcreteTypeInfoParam*) segment->addressNoLock(storageArray);
+            concreteType->interfaces.buffer     = addrArray;
+            segment->addInitPtr(OFFSETOF(concreteType->interfaces.buffer), storageArray);
+            for (int param = 0; param < concreteType->interfaces.count; param++)
+            {
+                SWAG_CHECK(typeTable->makeConcreteParam(baseContext, addrArray + param, storageArray, realType->interfaces[param], cflags));
+
+                // Compute the storage of the interface for swag_runtime_interfaceof
+                // Not needed if we are computing a compiler type
+                if (!(cflags & CONCRETE_FOR_COMPILER))
+                {
+                    uint32_t fieldOffset = offsetof(ConcreteTypeInfoParam, value);
+                    uint32_t valueOffset = storageArray + fieldOffset;
+                    segment->addInitPtr(valueOffset, realType->interfaces[param]->offset, SegmentKind::Constant);
+                }
+
+                storageArray += sizeof(ConcreteTypeInfoParam);
+            }
         }
     }
 
-    typeTable->tableJobDone(this);
+    // Second pass on interfaces.
+    // We do not want that pass to occur while locking the type segment, because this pass will lock also the constant segment
+    // (by calling constantSegment.address), and this can create a dead lock with SemanticJob::storeToSegmentNoLock (which first
+    // lock the constant segment, and then can lock the type segment for the 'any' type)
+    //
+    // This pass is used to store the address of each function of the interface in the 'value' field of the ConcreteTypeInfoParam.
+    if (concreteType->interfaces.count && !(cflags & CONCRETE_FOR_COMPILER))
+    {
+        ConcreteTypeInfoParam* addrArray = (ConcreteTypeInfoParam*) concreteType->interfaces.buffer;
+        for (int param = 0; param < concreteType->interfaces.count; param++)
+            addrArray[param].value = module->constantSegment.address(realType->interfaces[param]->offset);
+    }
+
+    // Job is done, remove it from the map
+    {
+        unique_lock lk1(segment->mutex);
+        typeTable->tableJobDone(this);
+    }
+
     return true;
 }
 
