95d778d1142f08087215fd4959bcc9ed481436ba
cb <christophe.beaudet@gmail.com>
2020-09-28 09:33:26 +0200

    remove main from cruntime

--- libc/win32/crt.cpp
+++ libc/win32/crt.cpp
@@ -6,21 +6,9 @@
 EXTERN_C
 {
     int _fltused = 0;
-
     void __chkstk()
     {
     }
-
-    /*void mainCRTStartup()
-    {
-        void swag_runtime_main(const char* cmdLine);
-        swag_runtime_main(GetCommandLineA());
-    }
-
-    int _DllMainCRTStartup(void*, int, void*)
-    {
-        return 1;
-    }*/
 }
 
 #endif // SWAG_RUNTIME_COMPILER
