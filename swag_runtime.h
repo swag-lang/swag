a608fa33a3a785e0f46dd59c4fe7a50328483eec
cb <christophe.beaudet@gmail.com>
2020-09-28 11:35:16 +0200

    clean tls cruntime

--- swag_runtime.h
+++ swag_runtime.h
@@ -1,8 +1,4 @@
 #pragma once
 #include "libc/stdint.h"
 
-extern "C" SwagU64 swag_runtime_tlsAlloc();
-extern "C" void    swag_runtime_tlsSetValue(SwagU64 id, void* value);
-extern "C" void*   swag_runtime_tlsGetValue(SwagU64 id);
-extern "C" void*   swag_runtime_memcpy(void* destination, const void* source, size_t size);
-
+extern "C" void* swag_runtime_memcpy(void* destination, const void* source, size_t size);
