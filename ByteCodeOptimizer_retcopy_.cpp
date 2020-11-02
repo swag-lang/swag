94b208341e097a345ce68140053d0a25bf475490
cb <christophe.beaudet@gmail.com>
2020-11-02 11:00:13 +0100

    optim bc

--- ByteCodeOptimizer_retcopy_.cpp
+++ ByteCodeOptimizer_retcopy_.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "ByteCodeOptimizer.h"
 #include "SourceFile.h"
+#include "AstNode.h"
 
 // When a function returns something by copy, this is first moved to a temporary place on the stack of the caller,
 // then this is moved back to the variable of the caller if there's an affectation.
@@ -41,15 +42,32 @@ void ByteCodeOptimizer::optimizePassRetCopy(ByteCodeOptContext* context)
                 // Change the original stack pointer offset to reference the variable instead of the temporary
                 // copy
                 auto orgOffset = ipOrg->b.u32;
-                ipOrg->b.u32 = ip->b.u32;
+                ipOrg->b.u32   = ip->b.u32;
+
+                // Is there a corresponding drop in the scope ?
+                bool hasDrop = false;
+                for (auto& toDrop : ipOrg->node->ownerScope->symTable.structVarsToDrop)
+                {
+                    if (toDrop.storageOffset == orgOffset && toDrop.typeStruct)
+                    {
+                        hasDrop = true;
+                        break;
+                    }
+                }
 
                 // Remove the MakeStackPointer
                 // For now we replace it with a reinit of the variable, because of opDrop that can be called
                 // on it.
-                //setNop(context, ip);
-                ip->op = ByteCodeOp::SetZeroStackX;
-                ip->a.u32 = orgOffset;
-                ip->b.u32 = ip[1].c.u32; // Copy the size from the following memcpy
+                if (hasDrop)
+                {
+                    ip->op    = ByteCodeOp::SetZeroStackX;
+                    ip->a.u32 = orgOffset;
+                    ip->b.u32 = ip[1].c.u32; // Copy the size from the following memcpy
+                }
+                else
+                {
+                    setNop(context, ip);
+                }
 
                 // Remove the memcpy
                 setNop(context, ip + 1);
