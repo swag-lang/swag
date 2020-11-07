fc505315572865574f127a2c6b1899eefa3cc67c
cb <christophe.beaudet@gmail.com>
2020-11-07 07:57:26 +0100

    clean

--- ByteCodeRun.cpp
+++ ByteCodeRun.cpp
@@ -480,8 +480,6 @@ inline bool ByteCodeRun::executeInstruction(ByteCodeRunContext* context, ByteCod
     }
     case ByteCodeOp::ForeignCall:
     {
-        if (ip->node->name == "tcf_toto20")
-            ip=ip;
         ffiCall(context, ip);
         break;
     }
