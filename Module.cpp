00d822483ede85726c75e023ff02efea4ab69c4e
cb <christophe.beaudet@gmail.com>
2020-11-29 10:02:42 +0100

    reduce mem (allocate backend)

--- Module.cpp
+++ Module.cpp
@@ -133,7 +133,7 @@ void Module::allocateBackend()
 
     // Allocate backend, even if we do not want to output, because the backend can be used
     // to know if a build is necessary
-    if (!hasUnittestError)
+    if (!hasUnittestError && buildPass >= BuildPass::Backend)
     {
         switch (g_CommandLine.backendType)
         {
