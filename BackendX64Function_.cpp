8c211fc37c3f90428e4be1d7654ccd7b6194f573
cb <christophe.beaudet@gmail.com>
2020-12-22 17:19:42 +0100

    mmm timers étranges

--- BackendX64Function_.cpp
+++ BackendX64Function_.cpp
@@ -1917,6 +1917,8 @@ bool BackendX64::emitFunctionBody(const BuildParameters& buildParameters, Module
             break;
         }
 
+        LARGE_INTEGER
+
         case ByteCodeOp::PushRVParam:
             pushRVParams.push_back({ip->a.u32, ip->b.u32});
             break;
