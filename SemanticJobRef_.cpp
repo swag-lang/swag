4d09f1c910c7746dca090c925613012da523d603
cb <christophe.beaudet@gmail.com>
2020-12-24 08:35:23 +0100

    start makecallback

--- SemanticJobRef_.cpp
+++ SemanticJobRef_.cpp
@@ -34,13 +34,24 @@ bool SemanticJob::resolveMakePointer(SemanticContext* context)
     // Lambda
     if (child->resolvedSymbolName->kind == SymbolKind::Function)
     {
-        SWAG_VERIFY(!(child->resolvedSymbolOverload->node->attributeFlags & ATTRIBUTE_INLINE), context->report({child, "cannot take address of an inline function"}));
+        auto funcNode = child->resolvedSymbolOverload->node;
+        SWAG_VERIFY(!(funcNode->attributeFlags & ATTRIBUTE_MACRO), context->report({child, "cannot take address of a macro"}));
+        SWAG_VERIFY(!(funcNode->attributeFlags & ATTRIBUTE_MIXIN), context->report({child, "cannot take address of a mixin"}));
+        SWAG_VERIFY(!(funcNode->attributeFlags & ATTRIBUTE_INLINE), context->report({child, "cannot take address of an inline function"}));
 
-        auto lambdaType    = child->typeInfo->clone();
-        lambdaType->kind   = TypeInfoKind::Lambda;
-        lambdaType->sizeOf = sizeof(void*);
-        node->typeInfo     = lambdaType;
-        node->byteCodeFct  = ByteCodeGenJob::emitMakeLambda;
+        if (funcNode->attributeFlags & ATTRIBUTE_CALLBACK)
+        {
+            node->typeInfo    = g_TypeMgr.typeInfoConstPVoid;
+            node->byteCodeFct = ByteCodeGenJob::emitMakeCallback;
+        }
+        else
+        {
+            auto lambdaType    = child->typeInfo->clone();
+            lambdaType->kind   = TypeInfoKind::Lambda;
+            lambdaType->sizeOf = sizeof(void*);
+            node->typeInfo     = lambdaType;
+            node->byteCodeFct  = ByteCodeGenJob::emitMakeLambda;
+        }
     }
 
     // Expression
@@ -57,7 +68,7 @@ bool SemanticJob::resolveMakePointer(SemanticContext* context)
             TypeInfoPointer* typeInfoPtr = CastTypeInfo<TypeInfoPointer>(typeInfo, TypeInfoKind::Pointer);
             if (!typeInfoPtr->isConst())
             {
-                ptrType = (TypeInfoPointer*)typeInfoPtr->clone();
+                ptrType = (TypeInfoPointer*) typeInfoPtr->clone();
                 ptrType->ptrCount++;
                 ptrType->computeName();
                 done = true;
