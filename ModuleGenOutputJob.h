d02afe9c029bb52fbb74dcdae8c025ff97b7aa1e
cb <christophe.beaudet@gmail.com>
2021-01-16 08:09:52 +0100

    ModuleGenOutputJob is io

--- ModuleGenOutputJob.h
+++ ModuleGenOutputJob.h
@@ -5,6 +5,11 @@ struct Module;
 
 struct ModuleGenOutputJob : public Job
 {
+    ModuleGenOutputJob()
+    {
+        flags |= JOB_IS_IO;
+    }
+
     JobResult execute() override;
 
     BuildParameters     buildParameters;
