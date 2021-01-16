4c2aa1c92eece909932ea09593abbad910613326
cb <christophe.beaudet@gmail.com>
2021-01-16 17:52:48 +0100

    selectif slice

--- ByteCodeRun.cpp
+++ ByteCodeRun.cpp
@@ -356,14 +356,30 @@ bool ByteCodeRun::getVariadicSI(ByteCodeRunContext* context, ByteCodeInstruction
 bool ByteCodeRun::executeIsConstExprSI(ByteCodeRunContext* context, ByteCodeInstruction* ip)
 {
     auto solved = (SymbolOverload*) ip->d.pointer;
+
+    // Variadic
+    /////////////////////////////////////////
     if (solved->typeInfo->kind == TypeInfoKind::Variadic || solved->typeInfo->kind == TypeInfoKind::TypedVariadic)
         return getVariadicSI(context, ip, nullptr, nullptr);
 
     uint32_t paramIdx   = ip->c.u32;
     auto     callParams = context->callerContext->selectIfParameters;
     SWAG_ASSERT(callParams && paramIdx < callParams->childs.size());
+    auto child = callParams->childs[paramIdx];
 
-    if (callParams->childs[paramIdx]->flags & (AST_VALUE_COMPUTED | AST_CONST_EXPR))
+    // Slice
+    /////////////////////////////////////////
+    if (solved->typeInfo->kind == TypeInfoKind::Slice)
+    {
+        if (child->typeInfo->kind == TypeInfoKind::Array)
+            return true;
+        if (child->typeInfo->kind == TypeInfoKind::TypeListArray)
+            return true;
+    }
+
+    // The rest : flags
+    /////////////////////////////////////////
+    if (child->flags & (AST_VALUE_COMPUTED | AST_CONST_EXPR))
         return true;
 
     return false;
@@ -385,15 +401,40 @@ void ByteCodeRun::executeGetFromStackSI(ByteCodeRunContext* context, ByteCodeIns
     SWAG_ASSERT(callParams);
     auto registersRC = context->registersRC[context->curRC].buffer;
 
-    // Be sure value has been computed
+    // Variadic
+    /////////////////////////////////////////
     if (solved->typeInfo->kind == TypeInfoKind::Variadic || solved->typeInfo->kind == TypeInfoKind::TypedVariadic)
     {
         getVariadicSI(context, ip, &registersRC[ip->a.u32], &registersRC[ip->b.u32]);
         return;
     }
 
-    SWAG_ASSERT(paramIdx < callParams->childs.size());
     auto child = callParams->childs[paramIdx];
+
+    // Slice
+    /////////////////////////////////////////
+    if (solved->typeInfo->kind == TypeInfoKind::Slice)
+    {
+        if (child->typeInfo->kind == TypeInfoKind::Array)
+        {
+            auto typeArray                 = CastTypeInfo<TypeInfoArray>(child->typeInfo, TypeInfoKind::Array);
+            registersRC[ip->a.u32].pointer = nullptr;
+            registersRC[ip->b.u32].u64     = typeArray->totalCount;
+            return;
+        }
+
+        if (child->typeInfo->kind == TypeInfoKind::TypeListArray)
+        {
+            auto typeList                  = CastTypeInfo<TypeInfoList>(child->typeInfo, TypeInfoKind::TypeListArray);
+            registersRC[ip->a.u32].pointer = nullptr;
+            registersRC[ip->b.u32].u64     = typeList->subTypes.size();
+            return;
+        }
+    }
+
+    // Native
+    /////////////////////////////////////////
+    SWAG_ASSERT(paramIdx < callParams->childs.size());
     if (solved->typeInfo->kind == TypeInfoKind::Native)
     {
         switch (solved->typeInfo->nativeType)
