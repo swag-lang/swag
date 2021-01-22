cb57a236714f86512d92f6c9c150eaad67abb225
cb <christophe.beaudet@gmail.com>
2021-01-22 20:35:49 +0100

    workspace_watch_.cpp

--- src/Workspace.cpp
+++ src/Workspace.cpp
@@ -808,22 +808,3 @@ bool Workspace::build()
 
     return result;
 }
-
-bool Workspace::watch()
-{
-    setup();
-
-    if (g_CommandLine.verbose)
-        g_Log.verbose(format("=> watching workspace '%s'", workspacePath.string().c_str()));
-
-    CommandLineParser cmdParser;
-    cmdParser.setup(&g_CommandLine);
-    auto cmdLine = cmdParser.buildString(false);
-
-    OS::watch([&](const string& moduleName) {
-        uint32_t errors = 0;
-        OS::doProcess(format("swag.exe test %s", cmdLine.c_str()), g_Workspace.workspacePath.string(), g_CommandLine.verbose, errors);
-    });
-
-    return true;
-}
---
+++ src/Workspace_watch_.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "Workspace.h"
+#include "ModuleBuildJob.h"
+#include "CommandLineParser.h"
+
+bool Workspace::watch()
+{
+    setup();
+
+    if (g_CommandLine.verbose)
+        g_Log.verbose(format("=> watching workspace '%s'", workspacePath.string().c_str()));
+
+    CommandLineParser cmdParser;
+    cmdParser.setup(&g_CommandLine);
+    auto cmdLine = cmdParser.buildString(false);
+
+    OS::watch([&](const string& moduleName) {
+        uint32_t errors = 0;
+        OS::doProcess(format("swag.exe test %s", cmdLine.c_str()), g_Workspace.workspacePath.string(), g_CommandLine.verbose, errors);
+    });
+
+    return true;
+}
