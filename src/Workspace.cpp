c76ffd506ce78c5f9d1252875b9a060665b8a6a4
cb <christophe.beaudet@gmail.com>
2021-01-22 18:51:11 +0100

    refac commands, start

--- src/Workspace.cpp
+++ src/Workspace.cpp
@@ -5,7 +5,6 @@
 #include "ModuleSemanticJob.h"
 #include "EnumerateModuleJob.h"
 #include "ModuleBuildJob.h"
-#include "Os.h"
 #include "CommandLineParser.h"
 #include "Module.h"
 #include "TypeManager.h"
@@ -416,28 +415,6 @@ void Workspace::setup()
     g_ThreadMgr.init();
 }
 
-void Workspace::cleanFolderContent(const fs::path& path)
-{
-    if (g_CommandLine.cleanLog)
-        return;
-
-    OS::visitFiles(path.string().c_str(), [&](const char* cFileName) {
-        auto folder = path.string() + "/";
-        folder += cFileName;
-        try
-        {
-            fs::remove_all(folder);
-        }
-        catch (...)
-        {
-            g_Log.error(format("fatal error: cannot delete file '%s'", folder.c_str()));
-            exit(-1);
-        }
-    });
-
-    fs::remove_all(path);
-}
-
 void Workspace::setupCachePath()
 {
     // Cache directory
@@ -830,99 +807,6 @@ bool Workspace::build()
         g_Log.messageHeaderCentered("Done", format("%.3fs", OS::timerToSeconds(g_Stats.totalTime.load())));
 
     return result;
-}
-
-void Workspace::cleanPublic(const fs::path& basePath)
-{
-    if (fs::exists(basePath))
-    {
-        OS::visitFolders(basePath.string().c_str(), [&, this](const char* folder) {
-            auto path = basePath.string() + folder;
-            path += "/";
-            path += SWAG_PUBLIC_FOLDER;
-            path += "/";
-            path = normalizePath(path);
-            if (fs::exists(path))
-            {
-                // Clean all targets
-                OS::visitFolders(path.c_str(), [&, this](const char* folder) {
-                    auto cfgpath = path + folder;
-                    cfgpath      = normalizePath(cfgpath);
-                    if (fs::exists(path))
-                    {
-                        g_Log.messageHeaderCentered("Cleaning", cfgpath);
-                        cleanFolderContent(cfgpath);
-                    }
-                });
-
-                // Clean public folder itself
-                g_Log.messageHeaderCentered("Cleaning", path);
-                cleanFolderContent(path);
-            }
-        });
-    }
-}
-
-void Workspace::clean()
-{
-    setup();
-
-    // Clean all output folders
-    targetPath = workspacePath;
-    targetPath.append(SWAG_OUTPUT_FOLDER);
-    targetPath.append("/");
-    if (fs::exists(targetPath))
-    {
-        OS::visitFolders(targetPath.string().c_str(), [this](const char* folder) {
-            auto path = targetPath.string() + folder;
-            path      = normalizePath(path);
-            g_Log.messageHeaderCentered("Cleaning", path);
-            cleanFolderContent(path);
-        });
-    }
-
-    // Clean all cache folders for the given workspace
-    setupCachePath();
-    cachePath.append(SWAG_CACHE_FOLDER);
-    cachePath.append("/");
-    if (fs::exists(cachePath))
-    {
-        OS::visitFolders(cachePath.string().c_str(), [this](const char* folder) {
-            auto wkPath = workspacePath.filename().string() + "-";
-
-            // We sure to only clean for the given workspace
-            if (strstr(folder, wkPath.c_str()) == folder)
-            {
-                auto path = cachePath.string() + folder;
-                path      = normalizePath(path);
-                g_Log.messageHeaderCentered("Cleaning", path);
-                cleanFolderContent(path);
-            }
-        });
-    }
-
-    // Clean all public folders of the workspace modules
-    cleanPublic(modulesPath);
-    cleanPublic(examplesPath);
-    cleanPublic(testsPath);
-
-    // Clean the full content of the dependency path
-    if (g_CommandLine.cleanDep)
-    {
-        if (fs::exists(dependenciesPath))
-        {
-            auto path = normalizePath(dependenciesPath.string());
-            g_Log.messageHeaderCentered("Cleaning", path);
-            if (!g_CommandLine.cleanLog)
-                fs::remove_all(dependenciesPath);
-        }
-    }
-
-    // Otherwise just clean tmp files in it
-    else
-        cleanPublic(dependenciesPath);
-
-    g_Log.messageHeaderCentered("Done", "");
 }
 
 bool Workspace::watch()
---
+++ src/Workspace_clean_.cpp
@@ -0,0 +1,118 @@
+#include "pch.h"
+#include "Workspace.h"
+#include "OS.h"
+
+void Workspace::cleanFolderContent(const fs::path& path)
+{
+    if (g_CommandLine.cleanLog)
+        return;
+
+    OS::visitFiles(path.string().c_str(), [&](const char* cFileName) {
+        auto folder = path.string() + "/";
+        folder += cFileName;
+        try
+        {
+            fs::remove_all(folder);
+        }
+        catch (...)
+        {
+            g_Log.error(format("fatal error: cannot delete file '%s'", folder.c_str()));
+            exit(-1);
+        }
+    });
+
+    fs::remove_all(path);
+}
+
+void Workspace::cleanPublic(const fs::path& basePath)
+{
+    if (fs::exists(basePath))
+    {
+        OS::visitFolders(basePath.string().c_str(), [&, this](const char* folder) {
+            auto path = basePath.string() + folder;
+            path += "/";
+            path += SWAG_PUBLIC_FOLDER;
+            path += "/";
+            path = normalizePath(path);
+            if (fs::exists(path))
+            {
+                // Clean all targets
+                OS::visitFolders(path.c_str(), [&, this](const char* folder) {
+                    auto cfgpath = path + folder;
+                    cfgpath      = normalizePath(cfgpath);
+                    if (fs::exists(path))
+                    {
+                        g_Log.messageHeaderCentered("Cleaning", cfgpath);
+                        cleanFolderContent(cfgpath);
+                    }
+                });
+
+                // Clean public folder itself
+                g_Log.messageHeaderCentered("Cleaning", path);
+                cleanFolderContent(path);
+            }
+        });
+    }
+}
+
+void Workspace::clean()
+{
+    setup();
+
+    // Clean all output folders
+    targetPath = workspacePath;
+    targetPath.append(SWAG_OUTPUT_FOLDER);
+    targetPath.append("/");
+    if (fs::exists(targetPath))
+    {
+        OS::visitFolders(targetPath.string().c_str(), [this](const char* folder) {
+            auto path = targetPath.string() + folder;
+            path      = normalizePath(path);
+            g_Log.messageHeaderCentered("Cleaning", path);
+            cleanFolderContent(path);
+        });
+    }
+
+    // Clean all cache folders for the given workspace
+    setupCachePath();
+    cachePath.append(SWAG_CACHE_FOLDER);
+    cachePath.append("/");
+    if (fs::exists(cachePath))
+    {
+        OS::visitFolders(cachePath.string().c_str(), [this](const char* folder) {
+            auto wkPath = workspacePath.filename().string() + "-";
+
+            // We sure to only clean for the given workspace
+            if (strstr(folder, wkPath.c_str()) == folder)
+            {
+                auto path = cachePath.string() + folder;
+                path      = normalizePath(path);
+                g_Log.messageHeaderCentered("Cleaning", path);
+                cleanFolderContent(path);
+            }
+        });
+    }
+
+    // Clean all public folders of the workspace modules
+    cleanPublic(modulesPath);
+    cleanPublic(examplesPath);
+    cleanPublic(testsPath);
+
+    // Clean the full content of the dependency path
+    if (g_CommandLine.cleanDep)
+    {
+        if (fs::exists(dependenciesPath))
+        {
+            auto path = normalizePath(dependenciesPath.string());
+            g_Log.messageHeaderCentered("Cleaning", path);
+            if (!g_CommandLine.cleanLog)
+                fs::remove_all(dependenciesPath);
+        }
+    }
+
+    // Otherwise just clean tmp files in it
+    else
+        cleanPublic(dependenciesPath);
+
+    g_Log.messageHeaderCentered("Done", "");
+}
