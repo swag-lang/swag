742704ad4ba7db0ad63d2515e66a64882a483402
cb <christophe.beaudet@gmail.com>
2021-02-18 10:37:21 +0100

    missing break

--- src/Diagnostic.cpp
+++ src/Diagnostic.cpp
@@ -38,6 +38,7 @@ void Diagnostic::report(bool verboseMode) const
         if (!verboseMode)
             g_Log.setColor(LogColor::DarkYellow);
         g_Log.print("callstack: ");
+        break;
     case DiagnosticLevel::TraceError:
         if (!verboseMode)
             g_Log.setColor(LogColor::DarkYellow);
