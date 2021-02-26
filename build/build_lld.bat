0a271f5d5cd492fdca71a4a344559166ea68ff3a
cb <christophe.beaudet@gmail.com>
2021-02-26 10:51:16 +0100

    move build bat to /build

--- build/build_lld.bat
+++ build/build_lld.bat
@@ -4,4 +4,4 @@ cd "../llvm/lld/build"
 cmake -G "Visual Studio 16 2019" ..
 %MSBUILD% lld.sln /p:Configuration="Debug" -m -verbosity:m
 %MSBUILD% lld.sln /p:Configuration="Release" -m -verbosity:m
-cd "../../../tools"
+cd "../../../build"
--- build/build_llvm.bat
+++ build/build_llvm.bat
@@ -5,4 +5,4 @@ cd "f:/swag/llvm/llvm/build"
 cmake -G "Visual Studio 16 2019" -DCMAKE_ASM_COMPILER=%MSCL% ..
 %MSBUILD% llvm.sln /p:Configuration="Debug" -m -verbosity:m
 %MSBUILD% llvm.sln /p:Configuration="Release" -m -verbosity:m
-cd "../../../tools"
+cd "../../../build"
