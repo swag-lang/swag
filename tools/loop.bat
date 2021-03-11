c85cbb87756bde9699fbdf35bb876795966a5e74
cb <christophe.beaudet@gmail.com>
2021-03-11 13:06:42 +0100

    loop

--- tools/loop.bat
+++ tools/loop.bat
@@ -1,4 +1,4 @@
 :loop
-swag test -w:../swag/tests --backend:x64 --rebuild --devmode --cfg:fast-debug %1 %2 %3 %4
-swag test -w:../swag/std   --backend:x64 --rebuild --devmode --cfg:fast-debug %1 %2 %3 %4
+swag test -w:../swag/tests --backend:x64 --rebuild --devmode %1 %2 %3 %4
+swag test -w:../swag/std   --backend:x64 --rebuild --devmode %1 %2 %3 %4
 goto loop
