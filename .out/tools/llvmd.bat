77c88745f644a23a507045f3f09d9218867ce546
cb <christophe.beaudet@gmail.com>
2021-04-05 09:41:48 +0200

    rename howto => reference

--- .out/tools/llvmd.bat
+++ .out/tools/llvmd.bat
@@ -1,5 +1,5 @@
 swag_d test -w:../tests --backend:llvm --cfg:debug --rebuild %1 %2 %3 %4
 swag_d test -w:../std   --backend:llvm --cfg:debug --rebuild %1 %2 %3 %4
-swag_d test -w:../howto --backend:llvm --cfg:debug --rebuild %1 %2 %3 %4
+swag_d test -w:../reference --backend:llvm --cfg:debug --rebuild %1 %2 %3 %4
 swag_d get  -w:../examples
 swag_d test -w:../examples --backend:llvm --cfg:debug --rebuild %1 %2 %3 %4
--- .out/tools/x64.bat
+++ .out/tools/x64.bat
@@ -1,5 +1,5 @@
 swag test -w:../tests --backend:x64 --rebuild %1 %2 %3 %4
 swag test -w:../std --backend:x64 --rebuild %1 %2 %3 %4
-swag test -w:../howto --backend:x64 --rebuild %1 %2 %3 %4
+swag test -w:../reference --backend:x64 --rebuild %1 %2 %3 %4
 swag get  -w:../examples
 swag test -w:../examples --backend:x64 --rebuild %1 %2 %3 %4
