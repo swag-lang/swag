3a9101e30ddbed583b5c96cb2d57ad2ec0ef98d9
cb <christophe.beaudet@gmail.com>
2021-04-06 18:56:56 +0200

    core, add slice namespace

--- .out/tools/clean.bat
+++ .out/tools/clean.bat
@@ -1,4 +1,4 @@
-swag clean -w:../tests    --clean-dep %1 %2 %3 %4
-swag clean -w:../std      --clean-dep %1 %2 %3 %4
-swag clean -w:../howto    --clean-dep %1 %2 %3 %4
-swag clean -w:../examples --clean-dep %1 %2 %3 %4
+swag clean -w:../tests     --clean-dep %1 %2 %3 %4
+swag clean -w:../std       --clean-dep %1 %2 %3 %4
+swag clean -w:../reference --clean-dep %1 %2 %3 %4
+swag clean -w:../examples  --clean-dep %1 %2 %3 %4
