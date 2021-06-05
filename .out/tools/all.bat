8e6cd0090e12297186f86e37ca6bc44c939cb33d
cb <christophe.beaudet@gmail.com>
2021-06-05 09:59:31 +0200

    Pixel.FullTest

--- .out/tools/all.bat
+++ .out/tools/all.bat
@@ -1,5 +1,5 @@
-call x64.bat --cfg:debug
-call x64.bat --cfg:fast-debug
-call x64.bat --cfg:release
+call x64.bat --cfg:debug --tag:Pixel.FullTest
+call x64.bat --cfg:fast-debug --tag:Pixel.FullTest
+call x64.bat --cfg:release --tag:Pixel.FullTest
 call llvm.bat --cfg:debug --cfg-optim-bc:true
 
