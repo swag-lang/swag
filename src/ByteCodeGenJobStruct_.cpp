3d6c3275a834e496a5482613a31f5c0555e387ad
cb <christophe.beaudet@gmail.com>
2021-06-07 07:44:06 +0200

    bug 1

--- src/ByteCodeGenJobStruct_.cpp
+++ src/ByteCodeGenJobStruct_.cpp
@@ -996,8 +996,8 @@ void ByteCodeGenJob::emitRetValRef(ByteCodeGenContext* context, RegisterList& r0
     auto node = context->node;
     if (retVal)
     {
-        if (node->ownerInline)
-            emitInstruction(context, ByteCodeOp::CopyRBtoRA64, r0, node->ownerInline->resultRegisterRC);
+        if (node->resolvedSymbolOverload->node->ownerInline)
+            emitInstruction(context, ByteCodeOp::CopyRBtoRA64, r0, node->resolvedSymbolOverload->node->ownerInline->resultRegisterRC);
         else
             emitInstruction(context, ByteCodeOp::CopyRRtoRC, r0);
     }
