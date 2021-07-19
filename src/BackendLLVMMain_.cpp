f8935bd3061ea13c255b8dd0f6d1b59287e4d99f
cb <christophe.beaudet@gmail.com>
2021-07-19 08:55:00 +0200

    __chkstk on llvm

--- src/BackendLLVMMain_.cpp
+++ src/BackendLLVMMain_.cpp
@@ -32,13 +32,34 @@ bool BackendLLVM::emitOS(const BuildParameters& buildParameters)
             builder.CreateRet(builder.getInt32(1));
         }
 
+        // Stack probing. Must do it by end to avoid linking with vc runtime on windows.
         // void __chkstk()
+        if (g_CommandLine.arch == BackendArch::X86_64)
         {
-            llvm::FunctionType* FT = llvm::FunctionType::get(builder.getVoidTy(), {}, false);
-            llvm::Function*     F  = llvm::Function::Create(FT, llvm::Function::ExternalLinkage, "__chkstk", modu);
-            llvm::BasicBlock*   BB = llvm::BasicBlock::Create(context, "entry", F);
-            builder.SetInsertPoint(BB);
-            builder.CreateRetVoid();
+            Utf8 in;
+            in.append(R"(
+            __chkstk:
+            push    %rcx
+            push    %rax
+            cmp     $0x1000, %rax
+            lea     24(%rsp), %rcx
+            jb      1f
+            2:
+            sub     $0x1000, %rcx
+            test    %rcx, (%rcx)
+            sub     $0x1000, %rax
+            cmp     $0x1000, %rax
+            ja      2b
+            1:
+            sub     %rax, %rcx
+            test    %rcx, (%rcx)
+            pop     %rax
+            pop     %rcx
+            ret)");
+
+            in.append("\n");
+            in.append(".global __chkstk\n");
+            modu.setModuleInlineAsm(in.c_str());
         }
 
         // int _fltused = 0;
