f100c6870bdb5bd47d11ab5517734cfb2098a673
cb <christophe.beaudet@gmail.com>
2021-07-29 16:24:31 +0200

    loop

--- .out/tools/loop_dm.bat
+++ .out/tools/loop_dm.bat
@@ -1,4 +1,4 @@
 :loop
-swag_devmode test -w:../testsuite --backend:x64 --rebuild %1 %2 %3 %4
-rem call x64_dm.bat
+rem swag_devmode test -w:../testsuite --backend:x64 --rebuild %1 %2 %3 %4
+call x64_dm.bat
 goto loop
