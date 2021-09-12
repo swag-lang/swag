176fa82ea4d4c58c3d21398e0c74d7ce649fe13e
cb <christophe.beaudet@gmail.com>
2021-09-12 11:37:13 +0200

    mkrel

--- tools/mkrel.bat
+++ tools/mkrel.bat
@@ -1,7 +1,5 @@
 call clean.bat
 
-7z a rel.zip ../bin/swag.exe
-7z a rel.zip ../bin/swag.pdb
 7z a rel.zip LICENCE
 7z a rel.zip LICENCE.md
 
@@ -9,3 +7,6 @@ call clean.bat
 7z a rel.zip ../bin/*.dll -r
 7z a rel.zip ../bin/*.lib -r
 7z a rel.zip ../bin/*.pdb -r
+
+7z a rel.zip ../bin/swag.exe
+7z a rel.zip ../bin/swag.pdb
