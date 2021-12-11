699881e4d9f418f58cbab813feb1af15573c9c7c
swag-lang <swaglanggit@gmail.com>
2021-12-11 09:29:34 +0100

    err++

--- src/SemanticJob_Identifier.cpp
+++ src/SemanticJob_Identifier.cpp
@@ -2768,9 +2768,18 @@ bool SemanticJob::getUsingVar(SemanticContext* context, AstIdentifierRef* identi
         {
             if (dependentVar)
             {
-                Diagnostic diag{dep.node, g_E[Err0117]};
-                Diagnostic note{dependentVar, g_E[Nte0036], DiagnosticLevel::Note};
-                return context->report(diag, &note);
+                if (dep.node->specFlags & AST_SPEC_DECLPARAM_GENERATED_SELF)
+                {
+                    Diagnostic diag{dependentVar, g_E[Err0117]};
+                    Diagnostic note{dep.node, g_E[Nte0056], DiagnosticLevel::Note};
+                    return context->report(diag, &note);
+                }
+                else
+                {
+                    Diagnostic diag{dep.node, g_E[Err0117]};
+                    Diagnostic note{dependentVar, g_E[Nte0036], DiagnosticLevel::Note};
+                    return context->report(diag, &note);
+                }
             }
 
             dependentVar = dep.node;
