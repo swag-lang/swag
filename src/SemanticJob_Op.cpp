2ea9ceec8222f645a79d02b375f67241c6f916e0
swag-lang <swaglanggit@gmail.com>
2022-01-02 20:08:15 +0100

    [swag] opCast cannot be generic

--- src/SemanticJob_Op.cpp
+++ src/SemanticJob_Op.cpp
@@ -106,6 +106,10 @@ bool SemanticJob::checkFuncPrototypeOp(SemanticContext* context, AstFuncDecl* no
         SWAG_VERIFY(firstGen->typeInfo->isSame(g_TypeMgr->typeInfoBool, ISSAME_CAST), context->report({firstGen, Utf8::format(g_E[Err0074], name.c_str(), firstGen->typeInfo->getDisplayName().c_str())}));
         SWAG_VERIFY(node->attributeFlags & ATTRIBUTE_MACRO, context->report({node, g_E[Err0075]}));
     }
+    else if (name == g_LangSpec->name_opCast)
+    {
+        SWAG_VERIFY(!node->genericParameters, context->report({node, Utf8::format(g_E[Err0478], name.c_str())}));
+    }
     else
     {
         SWAG_VERIFY(!node->genericParameters || node->genericParameters->childs.size() == 1, context->report({node, Utf8::format(g_E[Err0450], name.c_str())}));
