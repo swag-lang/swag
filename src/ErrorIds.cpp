a2c906c28acf67431d1d54075718f7cad1b3d589
swag-lang <swaglanggit@gmail.com>
2022-01-09 09:06:29 +0100

    [swag] divide errlist.h

--- src/ErrorIds.cpp
+++ src/ErrorIds.cpp
@@ -3,5 +3,7 @@
 #undef SWAG_ERROR
 #define SWAG_ERROR(__n, __msg) __msg,
 const char* g_E[] = {
-#include "ErrorList.h"
+#include "ErrorList_Err.h"
+#include "ErrorList_Note.h"
+#include "ErrorList_Hint.h"
 };
--- src/ErrorIds.h
+++ src/ErrorIds.h
@@ -4,7 +4,9 @@
 #define SWAG_ERROR(__n, __msg) __n,
 enum g_V
 {
-#include "ErrorList.h"
+#include "ErrorList_Err.h"
+#include "ErrorList_Note.h"
+#include "ErrorList_Hint.h"
 };
 
 extern const char* g_E[];
