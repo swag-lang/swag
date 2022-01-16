ce90b7f55c711b96cc5875afba310be4ec9d1bab
swag-lang <swaglanggit@gmail.com>
2022-01-16 18:36:05 +0100

    [swag] add optim bc

--- src/ByteCodeOptimizer_Reduce.cpp
+++ src/ByteCodeOptimizer_Reduce.cpp
@@ -722,6 +722,29 @@ void ByteCodeOptimizer::reduceStack(ByteCodeOptContext* context, ByteCodeInstruc
     {
         setNop(context, ip + 1);
     }
+
+    // Init stack just before a return
+    if (ip[1].op == ByteCodeOp::Ret)
+    {
+        switch (ip[0].op)
+        {
+        case ByteCodeOp::SetAtStackPointer8:
+        case ByteCodeOp::SetAtStackPointer16:
+        case ByteCodeOp::SetAtStackPointer32:
+        case ByteCodeOp::SetAtStackPointer64:
+        case ByteCodeOp::SetAtStackPointer8x2:
+        case ByteCodeOp::SetAtStackPointer16x2:
+        case ByteCodeOp::SetAtStackPointer32x2:
+        case ByteCodeOp::SetAtStackPointer64x2:
+        case ByteCodeOp::SetZeroStack8:
+        case ByteCodeOp::SetZeroStack16:
+        case ByteCodeOp::SetZeroStack32:
+        case ByteCodeOp::SetZeroStack64:
+        case ByteCodeOp::SetZeroStackX:
+            setNop(context, ip);
+            break;
+        }
+    }
 }
 
 void ByteCodeOptimizer::reduceIncPtr(ByteCodeOptContext* context, ByteCodeInstruction* ip)
