64c8bc34e536509f3080653fbe005850a4433454
swag-lang <swaglanggit@gmail.com>
2022-03-14 10:02:15 +0100

    [swag] jobgourp.cpp

---
+++ src/JobGroup.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "JobGroup.h"
+
+Job* JobGroup::pickJob()
+{
+    if (!mutex.try_lock())
+        return nullptr;
+    if (jobs.empty())
+    {
+        mutex.unlock();
+        return nullptr;
+    }
+
+    auto job = jobs.back();
+    jobs.pop_back();
+    job->jobGroup = this;
+    runningJobs++;
+    mutex.unlock();
+    return job;
+}
+
+void JobGroup::complete(Job* parentJob)
+{
+    ScopedLock lk(mutex);
+    while (runningJobs.load() > 0)
+        this_thread::yield();
+    for (auto j : jobs)
+    {
+        j->dependentJob = parentJob;
+        j->jobGroup     = nullptr;
+        j->flags &= ~JOB_IS_OPT;
+        parentJob->jobsToAdd.push_back(j);
+    }
+
+    jobs.clear();
+}
--- src/JobGroup.h
+++ src/JobGroup.h
@@ -9,28 +9,13 @@ struct JobGroup
     atomic<int>        runningJobs;
     VectorNative<Job*> jobs;
 
+    Job* pickJob();
+    void complete(Job* parentJob);
+
     void addJob(Job* job)
     {
         ScopedLock lk(mutex);
         jobs.push_back(job);
-    }
-
-    Job* pickJob()
-    {
-        if (!mutex.try_lock())
-            return nullptr;
-        if (jobs.empty())
-        {
-            mutex.unlock();
-            return nullptr;
-        }
-
-        auto job = jobs.back();
-        jobs.pop_back();
-        job->jobGroup = this;
-        runningJobs++;
-        mutex.unlock();
-        return job;
     }
 
     void doneJob()
@@ -53,22 +38,6 @@ struct JobGroup
             this_thread::yield();
         for (auto j : jobs)
             j->release();
-        jobs.clear();
-    }
-
-    void complete(Job* parentJob)
-    {
-        ScopedLock lk(mutex);
-        while (runningJobs.load() > 0)
-            this_thread::yield();
-        for (auto j : jobs)
-        {
-            j->dependentJob = parentJob;
-            j->jobGroup     = nullptr;
-            j->flags &= ~JOB_IS_OPT;
-            parentJob->jobsToAdd.push_back(j);
-        }
-
         jobs.clear();
     }
 };
