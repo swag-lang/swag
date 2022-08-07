7faa57d7aa62e9c6758df8a26976f93b5e6435cd
swag-lang <swaglanggit@gmail.com>
2022-08-07 12:14:13 +0200

    [swag] optim bc err

--- src/ByteCodeOptimizer_Reduce.cpp
+++ src/ByteCodeOptimizer_Reduce.cpp
@@ -9,6 +9,42 @@ void ByteCodeOptimizer::reduceErr(ByteCodeOptContext* context, ByteCodeInstructi
 {
     switch (ip[0].op)
     {
+    case ByteCodeOp::InternalClearErr:
+    {
+        auto ipScan = ip + 1;
+        while (ipScan->op != ByteCodeOp::Ret)
+        {
+            if (ipScan->op == ByteCodeOp::InternalHasErr ||
+                ipScan->op == ByteCodeOp::IntrinsicGetErr ||
+                ByteCode::isJump(ipScan) ||
+                (ipScan->flags & BCI_START_STMT))
+                break;
+
+            // An InternalClearErr followed by another one
+            if (ipScan->op == ByteCodeOp::InternalClearErr)
+            {
+                setNop(context, ip);
+                break;
+            }
+
+            // An InternalClearErr followed by a function call which will clear the error also
+            if (ipScan->op == ByteCodeOp::LocalCall ||
+                ipScan->op == ByteCodeOp::LambdaCall ||
+                ipScan->op == ByteCodeOp::ForeignCall)
+            {
+                TypeInfoFuncAttr* typeFuncBC = (TypeInfoFuncAttr*) ipScan->b.pointer;
+                if (typeFuncBC->flags & TYPEINFO_CAN_THROW)
+                {
+                    setNop(context, ip);
+                    break;
+                }
+            }
+
+            ipScan++;
+        }
+    }
+    break;
+
     case ByteCodeOp::InternalHasErr:
         // Has err followed by ret
         if (ip[1].op == ByteCodeOp::Ret)
