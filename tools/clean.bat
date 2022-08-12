ecb629c78f56e2d150c2db844f58abebb1175194
swag-lang <swaglanggit@gmail.com>
2022-08-12 18:24:31 +0200

    [swag] tools/clean clean script too

--- tools/clean.bat
+++ tools/clean.bat
@@ -2,3 +2,4 @@
 ..\bin\swag clean -w:../bin/std       --clean-dep %1 %2 %3 %4
 ..\bin\swag clean -w:../bin/reference --clean-dep %1 %2 %3 %4
 ..\bin\swag clean -w:../bin/examples  --clean-dep %1 %2 %3 %4
+..\bin\swag clean --script
