69d3e46ae3efa1f9eb1604495b3f77598540ac08
Jérémy <jyb@initiatives.fr>
2022-11-05 11:21:38 +0100

    website beta

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<?php include('html_start.php'); ?>
+<?php include('header.php'); ?>
+
+<div class="lg:m-auto lg:w-[70rem] pt-2 lg:pt-10 overflow-hidden p-5">
+
+    <div class="bg-amber-100 border-l-4 border-amber-300 p-3 rounded">Work in progress ! There's no "official" release yet.</div>
+
+    <div class="mt-5">Swag is a native and/or interpreted programming language made for fun because, let's be honest, C++ is now an horrible and ugly beast ! This is my third compiler (the other ones were developed for AAA game engines), but that one is by far the most advanced.</div>
+
+    <div class="mt-5">If you want to take a look at the language, this is <a href="/doc.php" class="text-blue-700">here</a>.</div>
+
+</div>
+
+<?php include('html_end.php'); ?>
