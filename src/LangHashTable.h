96be713379f9771f11a1a50849c7f6fb9228446a
swag-lang <swaglanggit@gmail.com>
2023-02-15 21:50:41 +0100

    [swag] langhashtable

--- src/LangHashTable.h
+++ src/LangHashTable.h
@@ -7,100 +7,63 @@ struct LangHashTable
     struct Entry
     {
         const char* key;
-        int         keyLen;
-        V           value;
+        uint32_t    keyLen;
         uint32_t    hash;
+        V           value;
     };
 
-    Entry*   buffer;
-    uint32_t allocated = 0;
-    uint32_t count;
     bool     firstLetter[256] = {0};
-    int      maxLetters       = 0;
+    Entry    buffer[N]        = {0};
+    uint32_t count            = 0;
+    uint32_t allocated        = N;
+    uint32_t minLetters       = UINT32_MAX;
+    uint32_t maxLetters       = 0;
 
     const V* find(const Utf8& key)
     {
         return find(key.buffer, key.count, 0);
     }
 
-    const V* find(const char* key, int keyLen, uint32_t crc)
+    const V* find(const char* key, uint32_t keyLen, uint32_t crc)
     {
-        if (!firstLetter[key[0]] || keyLen > maxLetters)
+        if (keyLen < minLetters || keyLen > maxLetters || !firstLetter[key[0]])
             return nullptr;
 
         if (!crc)
             crc = Crc32::compute((const uint8_t*) key, keyLen);
 
-        uint32_t idx = crc % allocated;
+        uint32_t idx = crc % N;
         while (buffer[idx].hash)
         {
             if (buffer[idx].hash == crc && buffer[idx].keyLen == keyLen && !strncmp(buffer[idx].key, key, keyLen))
                 return &buffer[idx].value;
-            idx = (idx + 1) % allocated;
+            idx = (idx + 1) % N;
         }
 
         return nullptr;
     }
 
-    void addElem(const char* key, int keyLen, const V& value, uint32_t crc = 0)
+    void add(const char* key, const V& value)
     {
-        if (!crc)
-            crc = Crc32::compute((const uint8_t*) key, keyLen);
+        SWAG_ASSERT(count != N);
+
+        uint32_t keyLen = (uint32_t) strlen(key);
+        auto     crc    = Crc32::compute((const uint8_t*) key, keyLen);
 
         // Find a free slot
-        uint32_t idx = crc % allocated;
+        uint32_t idx = crc % N;
         while (buffer[idx].hash)
         {
-            idx = (idx + 1) % allocated;
+            idx = (idx + 1) % N;
         }
 
         firstLetter[key[0]] = true;
+        minLetters          = min(minLetters, keyLen);
         maxLetters          = max(maxLetters, keyLen);
         buffer[idx].hash    = crc;
         buffer[idx].key     = key;
         buffer[idx].keyLen  = keyLen;
         buffer[idx].value   = value;
         count += 1;
-    }
-
-    void add(const char* key, const V& value)
-    {
-        // First allocation
-        if (!allocated)
-        {
-            count     = 0;
-            allocated = N;
-            buffer    = (Entry*) g_Allocator.alloc(allocated * sizeof(Entry));
-            memset(buffer, 0, allocated * sizeof(Entry));
-        }
-
-        // Need to grow the hash table, and add back the old values
-        else if (count >= allocated / 2)
-        {
-            auto oldAllocated = allocated;
-            auto oldBuffer    = buffer;
-            auto oldCount     = count;
-
-            allocated *= 2;
-            buffer = (Entry*) g_Allocator.alloc(allocated * sizeof(Entry));
-            memset(buffer, 0, allocated * sizeof(Entry));
-
-            count = 0;
-            for (uint32_t i = 0; i < oldAllocated; i++)
-            {
-                if (oldBuffer[i].hash)
-                {
-                    oldCount--;
-                    addElem(oldBuffer[i].key, oldBuffer[i].keyLen, oldBuffer[i].value, oldBuffer[i].hash);
-                    if (!oldCount)
-                        break;
-                }
-            }
-
-            g_Allocator.free(oldBuffer, oldAllocated * sizeof(Entry));
-        }
-
-        // Find a free slot
-        addElem(key, (int) strlen(key), value);
     }
 };
