6c4418e2d96ec8855e75397b660f0f5f760e8017
swag-lang <swaglanggit@gmail.com>
2023-02-16 08:49:20 +0100

    [swag] tokenizer

--- src/Tokenizer.cpp
+++ src/Tokenizer.cpp
@@ -123,49 +123,6 @@ void Tokenizer::eatChar(uint32_t c, unsigned offset)
     curBuffer += offset;
 }
 
-bool Tokenizer::doMultiLineComment(TokenParse& token)
-{
-    int countEmb = 1;
-    while (true)
-    {
-        auto c = readChar();
-        while (c && c != '*' && c != '/')
-            c = readChar();
-
-        if (!c)
-        {
-            location = token.startLocation;
-            location.column += 2;
-            return error(token, Err(Tkn0025), Hnt(Hnt0041));
-        }
-
-        if (c == '*')
-        {
-            unsigned offset;
-            c = peekChar(offset);
-            if (c == '/')
-            {
-                eatChar(c, offset);
-                countEmb--;
-                if (countEmb == 0)
-                    return true;
-            }
-
-            continue;
-        }
-
-        if (c == '/')
-        {
-            c = readChar();
-            if (c == '*')
-            {
-                countEmb++;
-                continue;
-            }
-        }
-    }
-}
-
 string Tokenizer::tokenToName(TokenId id)
 {
     switch (id)
---
+++ src/Tokenizer_Comment.cpp
@@ -0,0 +1,46 @@
+#include "pch.h"
+#include "Tokenizer.h"
+#include "ErrorIds.h"
+
+bool Tokenizer::doMultiLineComment(TokenParse& token)
+{
+    int countEmb = 1;
+    while (true)
+    {
+        auto c = readChar();
+        while (c && c != '*' && c != '/')
+            c = readChar();
+
+        if (!c)
+        {
+            location = token.startLocation;
+            location.column += 2;
+            return error(token, Err(Tkn0025), Hnt(Hnt0041));
+        }
+
+        if (c == '*')
+        {
+            unsigned offset;
+            c = peekChar(offset);
+            if (c == '/')
+            {
+                eatChar(c, offset);
+                countEmb--;
+                if (countEmb == 0)
+                    return true;
+            }
+
+            continue;
+        }
+
+        if (c == '/')
+        {
+            c = readChar();
+            if (c == '*')
+            {
+                countEmb++;
+                continue;
+            }
+        }
+    }
+}
