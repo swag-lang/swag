218e4c0bbb18bf642c1d3931879edd00f7070ed8
swag-lang <swaglanggit@gmail.com>
2023-02-17 08:08:56 +0100

    [swag] reduce assert (?)

--- src/SemanticJob.cpp
+++ src/SemanticJob.cpp
@@ -295,7 +295,9 @@ JobResult SemanticJob::execute()
         bool canDoSem = !(node->flags & AST_NO_SEMANTIC);
 
         // To be sure that a bytecode job is not running on those nodes !
-        SWAG_ASSERT(node->bytecodeState == AstNodeResolveState::Enter || (node->flags & (AST_VALUE_COMPUTED | AST_CONST_EXPR)));
+        SWAG_ASSERT(node->bytecodeState == AstNodeResolveState::Enter ||
+                    node->bytecodeState == AstNodeResolveState::PostChilds ||
+                    (node->flags & (AST_VALUE_COMPUTED | AST_CONST_EXPR)));
 
         // Some attribute flags must propagate from parent to childs, whatever
         inheritAttributesFromParent(node);
