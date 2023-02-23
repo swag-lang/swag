e28e0f3532099a5b9c9d6ae9679dd7797c222251
swag-lang <swaglanggit@gmail.com>
2023-02-23 08:31:21 +0100

    [tools] put img in test examples

--- tools/examples.bat
+++ tools/examples.bat
@@ -4,6 +4,8 @@
 ..\bin\swag run -w:..\bin\examples -m:wnd    --bytecode --args:"swag.test"
 ..\bin\swag run -w:..\bin\examples -m:captme --bytecode --args:"swag.test"
 ..\bin\swag run -w:..\bin\examples -m:draw   --bytecode --args:"swag.test"
+..\bin\swag run -w:..\bin\examples -m:img    --bytecode --args:"swag.test"
 ..\bin\swag run -w:..\bin\examples -m:wnd    --args:"swag.test"
 ..\bin\swag run -w:..\bin\examples -m:captme --args:"swag.test"
 ..\bin\swag run -w:..\bin\examples -m:draw   --args:"swag.test"
+..\bin\swag run -w:..\bin\examples -m:img    --args:"swag.test"
