64c870fdd74a3cc5b33040c51ac45ad4c7b46319
swag-lang <swaglanggit@gmail.com>
2023-03-26 19:06:34 +0200

    [swag] x64

--- src/BackendX64_Function.cpp
+++ src/BackendX64_Function.cpp
@@ -2387,28 +2387,61 @@ bool BackendX64::emitFunctionBody(const BuildParameters& buildParameters, Module
             pp.emit_Add64_RAX(ip->c.u64);
             pp.emit_Store64_Indirect(regOffset(ip->a.u32), RAX);
             break;
+
         case ByteCodeOp::GetIncFromStack64DeRef8:
-            pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
-            pp.emit_Add64_RAX(ip->c.u64);
-            pp.emit_LoadU8U64_Indirect(0, RAX, RAX);
+            if (ip->c.u64 <= 0x7FFFFFFF)
+            {
+                pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
+                pp.emit_LoadU8U64_Indirect((uint32_t) ip->c.u64, RAX, RAX);
+            }
+            else
+            {
+                pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
+                pp.emit_Add64_RAX(ip->c.u64);
+                pp.emit_LoadU8U64_Indirect(0, RAX, RAX);
+            }
             pp.emit_Store64_Indirect(regOffset(ip->a.u32), RAX);
             break;
         case ByteCodeOp::GetIncFromStack64DeRef16:
-            pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
-            pp.emit_Add64_RAX(ip->c.u64);
-            pp.emit_LoadU16U64_Indirect(0, RAX, RAX);
+            if (ip->c.u64 <= 0x7FFFFFFF)
+            {
+                pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
+                pp.emit_LoadU16U64_Indirect((uint32_t) ip->c.u64, RAX, RAX);
+            }
+            else
+            {
+                pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
+                pp.emit_Add64_RAX(ip->c.u64);
+                pp.emit_LoadU16U64_Indirect(0, RAX, RAX);
+            }
             pp.emit_Store64_Indirect(regOffset(ip->a.u32), RAX);
             break;
         case ByteCodeOp::GetIncFromStack64DeRef32:
-            pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
-            pp.emit_Add64_RAX(ip->c.u64);
-            pp.emit_Load32_Indirect(0, RAX, RAX);
+            if (ip->c.u64 <= 0x7FFFFFFF)
+            {
+                pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
+                pp.emit_Load32_Indirect((uint32_t) ip->c.u64, RAX, RAX);
+            }
+            else
+            {
+                pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
+                pp.emit_Add64_RAX(ip->c.u64);
+                pp.emit_Load32_Indirect(0, RAX, RAX);
+            }
             pp.emit_Store64_Indirect(regOffset(ip->a.u32), RAX);
             break;
         case ByteCodeOp::GetIncFromStack64DeRef64:
-            pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
-            pp.emit_Add64_RAX(ip->c.u64);
-            pp.emit_Load64_Indirect(0, RAX, RAX);
+            if (ip->c.u64 <= 0x7FFFFFFF)
+            {
+                pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
+                pp.emit_Load64_Indirect((uint32_t) ip->c.u64, RAX, RAX);
+            }
+            else
+            {
+                pp.emit_Load64_Indirect(offsetStack + ip->b.u32, RAX, RDI);
+                pp.emit_Add64_RAX(ip->c.u64);
+                pp.emit_Load64_Indirect(0, RAX, RAX);
+            }
             pp.emit_Store64_Indirect(regOffset(ip->a.u32), RAX);
             break;
 
