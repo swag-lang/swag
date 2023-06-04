b28b01a94ab46d8c7ceb1db754dc3a057df340ab
swag-lang <swaglanggit@gmail.com>
2023-06-04 06:52:31 +0200

    [web]

--- web/getting_started.php
+++ web/getting_started.php
@@ -10,7 +10,7 @@
 <p><a href="https://github.com/swag-lang/swag/releases">Download</a> the
 latest release from github, and unzip it in a folder. Of course a
 <em>SSD</em> is better.</p>
-<h4 id="under-windows-1011">Under windows 10/11</h4>
+<h3 id="under-windows-1011">Under windows 10/11</h3>
 <p>You should register the location of the swag compiler
 (<code>swag.exe</code>) in the PATH environment variable to be able to
 call it from everywhere.</p>
@@ -38,25 +38,32 @@ print “Hello world !”.</p>
 {
     @print(&quot;Hello world!\n&quot;)
 }</code></pre>
-<p>A workspace contains a predefined number of sub folders : *
-<code>modules/</code> contains all the modules (sub folders) of that
-workspace. * <code>output/</code> (generated) contains the result of the
-build (this is where the executable will be located). *
-<code>tests/</code> (optional) contains a list of test modules. *
-<code>examples/</code> (optional) contains a list of ‘examples’ modules.
-* <code>dependencies/</code> (generated) contains a list of external
-modules needed to compile the workspace.</p>
-<p>A module is also organized in a predefined way : *
-<code>moduleName/</code> the folder name of the module is used to create
-output files. * <code>src/</code> contains the source code. *
-<code>public/</code> (generated) will contain all the exports needed by
-other modules to use that one (in case of a dynamic library). *
-<code>publish/</code> contains additional files to use that module (like
-an external C dll).</p>
+<p>A workspace contains a predefined number of sub folders:</p>
+<ul>
+<li><code>modules/</code> contains all the modules (sub folders) of that
+workspace.</li>
+<li><code>output/</code> (generated) contains the result of the build
+(this is where the executable will be located).</li>
+<li><code>tests/</code> (optional) contains a list of test modules.</li>
+<li><code>examples/</code> (optional) contains a list of ‘examples’
+modules.</li>
+<li><code>dependencies/</code> (generated) contains a list of external
+modules needed to compile the workspace.</li>
+</ul>
+<p>A module is also organized in a predefined way:</p>
+<ul>
+<li><code>moduleName/</code> the folder name of the module is used to
+create output files.</li>
+<li><code>src/</code> contains the source code.</li>
+<li><code>public/</code> (generated) will contain all the exports needed
+by other modules to use that one (in case of a dynamic library).</li>
+<li><code>publish/</code> contains additional files to use that module
+(like an external C dll).</li>
+</ul>
 <p>A module always contains a special file named
 <code>module.swg</code>. This file is used to configure the module, and
 is <strong>mandatory</strong>.</p>
-<h4 id="to-compile-your-workspace">To compile your workspace</h4>
+<h3 id="to-compile-your-workspace">To compile your workspace</h3>
 <pre><code>$ swag build -w:first
             Workspace first [fast-debug-windows-x86_64]
              Building first
@@ -72,8 +79,8 @@ the workspace folder. This command will compile all modules in
       Running backend first
 Hello world!
                  Done 0.093s</code></pre>
-<h4 id="note-on-windows-defender-realtime-protection">Note on Windows
-Defender realtime protection</h4>
+<h3 id="note-on-windows-defender-realtime-protection">Note on Windows
+Defender realtime protection</h3>
 <p>It’s activated by default under Windows 10, and runs each time you
 launch an executable or a process. This can increase the compile time of
 your project, so consider excluding your Swag folder from it !</p>
@@ -81,20 +88,26 @@ your project, so consider excluding your Swag folder from it !</p>
 href="https://support.microsoft.com/en-us/windows/add-an-exclusion-to-windows-security-811816c0-4dfd-af4a-47e4-c301afe13b26#:~:text=Go%20to%20Start%20%3E%20Settings%20%3E%20Update,%2C%20file%20types%2C%20or%20process">Reference</a></p>
 <h1 id="content-of-the-swag-folder">Content of the Swag folder</h1>
 <p>The Swag folder contains the compiler <code>swag.exe</code>, but also
-a bunch of sub folders. * <code>examples/</code> is a workspace with
-various code examples (mostly for testing). * <code>reference/</code> is
-a workspace which contains an overview of the language, in the form of
-small tests. * <code>testsuite/</code> is a workspace which contains all
-the tests to debug the compiler. * <code>runtime/</code> contains the
-compiler runtime, which is included in all user modules. *
-<code>std/</code> is the <em>standard</em> workspace which contains the
-<em>standard</em> modules that come with the compiler. A big work in
-progress.</p>
+a bunch of sub folders.</p>
+<ul>
+<li><code>examples/</code> is a workspace with various code examples
+(mostly for testing).</li>
+<li><code>reference/</code> is a workspace which contains an overview of
+the language, in the form of small tests.</li>
+<li><code>testsuite/</code> is a workspace which contains all the tests
+to debug the compiler.</li>
+<li><code>runtime/</code> contains the compiler runtime, which is
+included in all user modules.</li>
+<li><code>std/</code> is the <a href="std.php">standard workspace</a>
+which contains all the standard modules that come with the compiler. A
+big work in progress.</li>
+</ul>
 <h1 id="the-swag-language">The Swag language</h1>
 <p>You should take a look at the <code>reference/</code> sub folder in
-the Swag directory. It contains the list of all that can be done with
-the language, in the form of small tests (in fact it’s not really
-exhaustive, but should be…).</p>
+the Swag directory, or to the corresponding <a
+href="language.php">documentation</a>. It contains the list of all that
+can be done with the language, in the form of small tests (in fact it’s
+not really exhaustive, but should be…).</p>
 <p>It’s a good starting point to familiarize yourself with the
 language.</p>
 <p>And as this is a normal Swag workspace, you could also build and test
@@ -102,7 +115,7 @@ it with <code>swag test -w:swag/reference</code>.</p>
 <p>You will also find some small examples (mostly written for tests) in
 <code>swag/bin/examples/modules</code>. To build and run one of them
 from the console, go to the workspace folder (<code>/examples</code>)
-and type for example <code>swag run -m:wnd</code></p>
+and type for example <code>swag run -m:wnd</code>.</p>
 </div>
 </div>
 <?php include('html_end.php'); ?>
