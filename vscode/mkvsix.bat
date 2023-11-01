f6c0d025a9c139d2c73e1cea7e80b88888af1a1e
swag-lang <swaglanggit@gmail.com>
2023-11-01 08:52:37 +0100

    vscode theme

--- vscode/mkvsix.bat
+++ vscode/mkvsix.bat
@@ -1,3 +1,6 @@
+REM in order to have 'vsce' to be installed, you need to:
+REM - go to https://nodejs.org/en to install Hode.js
+REM - open a command line, and type 'npm install -g vsce'
 xcopy ..\\web\\imgs\\swag_icon.png images\\ /Y
 xcopy ..\\web\\imgs\\syntax.png images\\ /Y
 vsce package
