72552aef935408aba664d32a8638b84ea7d93077
swag-lang <swaglanggit@gmail.com>
2023-11-01 10:32:45 +0100

    build .bat update

--- build/vs_build_cfg.bat
+++ build/vs_build_cfg.bat
@@ -1,4 +1,4 @@
 set MSBUILD="C:/Program Files/Microsoft Visual Studio/2022/Enterprise/MSBuild/Current/Bin/msbuild.exe"
-set MSCL="C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.35.32215/bin/Hostx64/x64/cl.exe"
+set MSCL="C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.37.32822/bin/Hostx64/x64/cl.exe"
 set CLANGFORMAT="C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/LLvm/x64/bin/clang-format.exe"
 set WSDK="C:/Program Files (x86)/Windows Kits/10/Lib/10.0.19041.0"
