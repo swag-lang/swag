f32617e52877d42b9c5afb79adceb42e41d53a70
swag-lang <swaglanggit@gmail.com>
2023-12-08 08:25:11 +0100

    .bat update

--- tools/examples.bat
+++ tools/examples.bat
@@ -1,6 +1,7 @@
 ..\bin\swag_devmode clean --ignore-bad-params -w:..\bin\examples %1 %2 %3 %4
 
-..\bin\swag run -w:..\bin\examples -m:wnd    --args:"swag.test" %1 %2 %3 %4
-..\bin\swag run -w:..\bin\examples -m:captme --args:"swag.test" %1 %2 %3 %4
-..\bin\swag run -w:..\bin\examples -m:draw   --args:"swag.test" %1 %2 %3 %4
-..\bin\swag run -w:..\bin\examples -m:img    --args:"swag.test" %1 %2 %3 %4
+..\bin\swag run -w:..\bin\examples -m:wnd     --args:"swag.test" %1 %2 %3 %4
+..\bin\swag run -w:..\bin\examples -m:captme  --args:"swag.test" %1 %2 %3 %4
+..\bin\swag run -w:..\bin\examples -m:draw    --args:"swag.test" %1 %2 %3 %4
+..\bin\swag run -w:..\bin\examples -m:img     --args:"swag.test" %1 %2 %3 %4
+..\bin\swag run -w:..\bin\examples -m:aoc2023 --args:"swag.test" %1 %2 %3 %4
