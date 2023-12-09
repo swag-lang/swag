d8806a207084b7fb9e6a4ba762e033b951e9527b
swag-lang <swaglanggit@gmail.com>
2023-12-09 16:52:29 +0100

    clean aoc

--- tools/aoc.bat
+++ tools/aoc.bat
@@ -1,5 +1,5 @@
-..\bin\swag run -w:..\bin\examples -m:aoc2022 --bytecode --cfg:release
+..\bin\swag run -w:..\bin\examples -m:aoc2022 --bytecode --cfg:release --num-cores:4
 ..\bin\swag run -w:..\bin\examples -m:aoc2022 --cfg:release
 
-..\bin\swag run -w:..\bin\examples -m:aoc2023 --bytecode --cfg:release
+..\bin\swag run -w:..\bin\examples -m:aoc2023 --bytecode --cfg:release --num-cores:4
 ..\bin\swag run -w:..\bin\examples -m:aoc2023 --cfg:release
