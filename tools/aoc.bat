b090fedcb8b5e84b1ca5c70ac7dd641b6c05a81f
swag-lang <swaglanggit@gmail.com>
2023-12-14 16:38:15 +0100

    2021 day1

--- tools/aoc.bat
+++ tools/aoc.bat
@@ -1,3 +1,6 @@
+..\bin\swag run -w:..\bin\examples -m:aoc2021 --bytecode --cfg:release --num-cores:4 %1
+..\bin\swag run -w:..\bin\examples -m:aoc2021 --cfg:debug %1
+
 ..\bin\swag run -w:..\bin\examples -m:aoc2022 --bytecode --cfg:release --num-cores:4 %1
 ..\bin\swag run -w:..\bin\examples -m:aoc2022 --cfg:debug %1
 
--- tools/examples.bat
+++ tools/examples.bat
@@ -4,5 +4,6 @@
 ..\bin\swag run -w:..\bin\examples -m:captme  --args:"swag.test" %1 %2 %3 %4
 ..\bin\swag run -w:..\bin\examples -m:draw    --args:"swag.test" %1 %2 %3 %4
 ..\bin\swag run -w:..\bin\examples -m:img     --args:"swag.test" %1 %2 %3 %4
+..\bin\swag run -w:..\bin\examples -m:aoc2021 --args:"swag.test" %1 %2 %3 %4
 ..\bin\swag run -w:..\bin\examples -m:aoc2022 --args:"swag.test" %1 %2 %3 %4
 ..\bin\swag run -w:..\bin\examples -m:aoc2023 --args:"swag.test" %1 %2 %3 %4
