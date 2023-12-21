33d16a08c395d2ee423fdd9623e26617ee40a876
swag-lang <swaglanggit@gmail.com>
2023-12-21 17:01:03 +0100

    regexp

--- tools/aoc.bat
+++ tools/aoc.bat
@@ -1,11 +1,11 @@
 ..\bin\swag run -w:..\bin\examples -m:aoc2020 --bytecode --cfg:release --num-cores:4 %1
 ..\bin\swag run -w:..\bin\examples -m:aoc2020 --cfg:debug %1
 
-REM ..\bin\swag run -w:..\bin\examples -m:aoc2021 --bytecode --cfg:release --num-cores:4 %1
-REM ..\bin\swag run -w:..\bin\examples -m:aoc2021 --cfg:debug %1
+..\bin\swag run -w:..\bin\examples -m:aoc2021 --bytecode --cfg:release --num-cores:4 %1
+..\bin\swag run -w:..\bin\examples -m:aoc2021 --cfg:debug %1
 
-REM ..\bin\swag run -w:..\bin\examples -m:aoc2022 --bytecode --cfg:release --num-cores:4 %1
-REM ..\bin\swag run -w:..\bin\examples -m:aoc2022 --cfg:debug %1
+..\bin\swag run -w:..\bin\examples -m:aoc2022 --bytecode --cfg:release --num-cores:4 %1
+..\bin\swag run -w:..\bin\examples -m:aoc2022 --cfg:debug %1
 
-REM ..\bin\swag run -w:..\bin\examples -m:aoc2023 --bytecode --cfg:release --num-cores:4 %1
-REM ..\bin\swag run -w:..\bin\examples -m:aoc2023 --cfg:debug %1
+..\bin\swag run -w:..\bin\examples -m:aoc2023 --bytecode --cfg:release --num-cores:4 %1
+..\bin\swag run -w:..\bin\examples -m:aoc2023 --cfg:debug %1
