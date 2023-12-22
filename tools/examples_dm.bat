65505e59a6d77a86632d98f3a8703adf2f2d3fb5
swag-lang <swaglanggit@gmail.com>
2023-12-22 08:21:06 +0100

    new test examples

--- tools/examples_dm.bat
+++ tools/examples_dm.bat
@@ -1,5 +1,8 @@
 ..\bin\swag_devmode clean --ignore-bad-params -w:..\bin\examples %1 %2 %3 %4
 
+..\bin\swag_devmode run -w:..\bin\examples -m:genetic --args:"swag.test" %1 %2 %3 %4
+..\bin\swag_devmode run -w:..\bin\examples -m:opengl1 --args:"swag.test" %1 %2 %3 %4
+..\bin\swag_devmode run -w:..\bin\examples -m:opengl2 --args:"swag.test" %1 %2 %3 %4
 ..\bin\swag_devmode run -w:..\bin\examples -m:wnd     --args:"swag.test" %1 %2 %3 %4
 ..\bin\swag_devmode run -w:..\bin\examples -m:captme  --args:"swag.test" %1 %2 %3 %4
 ..\bin\swag_devmode run -w:..\bin\examples -m:draw    --args:"swag.test" %1 %2 %3 %4
