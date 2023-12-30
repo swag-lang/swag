10276b733660c9db440dce314a2ab6a3def8ad67
swag-lang <swaglanggit@gmail.com>
2023-12-30 11:39:49 +0100

    4b job version

--- tools/aoc.bat
+++ tools/aoc.bat
@@ -1,2 +1,4 @@
-..\bin\swag run -w:..\bin\examples -m:aoc2015 --bytecode --cfg:release --num-cores:4 %1
-..\bin\swag run -w:..\bin\examples -m:aoc2015 --cfg:debug %1
+..\bin\swag run -w:..\bin\examples -m:aoc2015 --bytecode --cfg:release --num-cores:4 %1 %2
+..\bin\swag run -w:..\bin\examples -m:aoc2015 --cfg:release %1 %2
+
+..\bin\swag run -w:..\bin\examples -m:aoc2015 --cfg:debug %1 %2
