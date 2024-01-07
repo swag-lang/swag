188ab9c7d0df1771771b04471ce5f018e9f1cd54
swag-lang <swaglanggit@gmail.com>
2024-01-07 16:38:03 +0100

    2017 day 1

--- tools/aoc.bat
+++ tools/aoc.bat
@@ -1,3 +1,3 @@
-..\bin\swag run -w:..\bin\examples -m:aoc2016 --bytecode --cfg:debug --num-cores:4 %1 %2
-..\bin\swag run -w:..\bin\examples -m:aoc2016 --cfg:release %1 %2
-..\bin\swag run -w:..\bin\examples -m:aoc2016 --cfg:debug %1 %2
+..\bin\swag run -w:..\bin\examples -m:aoc2017 --bytecode --cfg:debug --num-cores:4 %1 %2
+..\bin\swag run -w:..\bin\examples -m:aoc2017 --cfg:release %1 %2
+..\bin\swag run -w:..\bin\examples -m:aoc2017 --cfg:debug %1 %2
