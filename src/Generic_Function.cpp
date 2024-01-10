34dc2410b44e06403c3d2146fea5eb20eb6f380f
swag-lang <swaglanggit@gmail.com>
2024-01-10 06:49:57 +0100

    refac

---
+++ src/Generic_Function.cpp
@@ -0,0 +1,306 @@
+#include "pch.h"
+#include "Generic.h"
+#include "Ast.h"
+#include "Diagnostic.h"
+#include "SemanticJob.h"
+#include "TypeManager.h"
+
+void Generic::instantiateSpecialFunc(SemanticContext* context, Job* structJob, CloneContext& cloneContext, AstFuncDecl** specialFct)
+{
+    auto funcNode = *specialFct;
+    if (!funcNode)
+        return;
+
+    // Clone original node
+    AstFuncDecl* newFunc = CastAst<AstFuncDecl>(funcNode->clone(cloneContext), AstNodeKind::FuncDecl);
+    if (newFunc->genericParameters)
+    {
+        newFunc->flags |= AST_IS_GENERIC;
+        newFunc->content->flags |= AST_NO_SEMANTIC;
+    }
+    else
+    {
+        newFunc->flags |= AST_FROM_GENERIC;
+        newFunc->content->flags &= ~AST_NO_SEMANTIC;
+    }
+
+    if (newFunc->validif)
+        newFunc->content->flags |= AST_NO_SEMANTIC;
+
+    newFunc->originalGeneric = funcNode;
+    Ast::addChildBack(funcNode->parent, newFunc);
+    *specialFct = newFunc;
+
+    // Generate and initialize a new type if the type is still generic
+    // The type is still generic if the doTypeSubstitution didn't find any type to change
+    // (for example if we have just generic value)
+    TypeInfoFuncAttr* newTypeFunc = CastTypeInfo<TypeInfoFuncAttr>(newFunc->typeInfo, newFunc->typeInfo->kind);
+    if (newTypeFunc->isGeneric())
+    {
+        newTypeFunc = CastTypeInfo<TypeInfoFuncAttr>(newFunc->typeInfo->clone(), newFunc->typeInfo->kind);
+        newTypeFunc->removeGenericFlag();
+        newFunc->typeInfo = newTypeFunc;
+    }
+
+    newTypeFunc->forceComputeName();
+
+    ScopedLock lk(newFunc->resolvedSymbolName->mutex);
+    auto       newJob = end(context, context->baseJob, newFunc->resolvedSymbolName, newFunc, false, cloneContext.replaceTypes);
+    structJob->dependentJobs.add(newJob);
+}
+
+bool Generic::instantiateFunction(SemanticContext* context, AstNode* genericParameters, OneGenericMatch& match, bool validif)
+{
+    auto node = context->node;
+
+    // We should have types to replace
+    bool noReplaceTypes = match.genericReplaceTypes.empty();
+    if (noReplaceTypes)
+    {
+        // If we do not have types to replace, perhaps we will be able to deduce them in case of a
+        // function call parameter
+        if (!node->findParent(AstNodeKind::FuncCallParam))
+        {
+            node->resolvedSymbolName     = match.symbolName;
+            node->resolvedSymbolOverload = match.symbolOverload;
+            node->typeInfo               = match.symbolOverload->typeInfo;
+
+            SWAG_CHECK(instantiateDefaultGenericFunc(context));
+            YIELD();
+        }
+    }
+
+    // If we instantiate with a tuple list (literal), then we must convert that to a proper struct decl and make
+    // a reference to it.
+    auto typeFunc = CastTypeInfo<TypeInfoFuncAttr>(match.symbolOverload->node->typeInfo, TypeInfoKind::FuncAttr);
+    for (auto& v : match.genericReplaceTypes)
+    {
+        if (v.second->isListTuple())
+        {
+            auto tpt = CastTypeInfo<TypeInfoList>(v.second, TypeInfoKind::TypeListTuple);
+            int  idx = 0;
+            for (auto p : match.parameters)
+            {
+                if (p->typeInfo == tpt)
+                {
+                    if (match.solvedParameters[idx]->typeInfo->kind != TypeInfoKind::Generic)
+                    {
+                        PushErrCxtStep ec(context, typeFunc->declNode, ErrCxtStepKind::HereIs, nullptr);
+                        auto           typeDest = CastTypeInfo<TypeInfoStruct>(match.solvedParameters[idx]->typeInfo, TypeInfoKind::Struct);
+                        SWAG_CHECK(Ast::convertLiteralTupleToStructType(context, typeDest, p));
+                        SWAG_ASSERT(context->result != ContextResult::Done);
+                        return true;
+                    }
+                    else
+                    {
+                        PushErrCxtStep ec(context, typeFunc->declNode, ErrCxtStepKind::HereIs, nullptr);
+                        auto           typeDest = TypeManager::convertTypeListToStruct(context, tpt, true);
+                        SWAG_CHECK(Ast::convertLiteralTupleToStructType(context, typeDest, p));
+                        SWAG_ASSERT(context->result != ContextResult::Done);
+                        return true;
+                    }
+                }
+
+                idx++;
+            }
+        }
+    }
+
+    // Types replacements
+    CloneContext cloneContext;
+    cloneContext.replaceTypes  = match.genericReplaceTypes;
+    cloneContext.replaceValues = match.genericReplaceValues;
+    cloneContext.replaceFrom   = match.genericReplaceFrom;
+
+    // We replace all types and generic types with undefined for now
+    if (noReplaceTypes)
+    {
+        for (auto p : typeFunc->genericParameters)
+        {
+            cloneContext.replaceTypes[p->typeInfo->name] = g_TypeMgr->typeInfoUndefined;
+        }
+
+        for (auto p : typeFunc->parameters)
+        {
+            if (p->typeInfo->isGeneric())
+                cloneContext.replaceTypes[p->typeInfo->name] = g_TypeMgr->typeInfoUndefined;
+        }
+    }
+
+    // :GenericConcreteAlias
+    // Make all types concrete in case of simple aliases
+    for (auto& p : cloneContext.replaceTypes)
+    {
+        p.second = TypeManager::concreteType(p.second, CONCRETE_ALIAS);
+    }
+
+    // Clone original node
+    auto overload = match.symbolOverload;
+    auto funcNode = overload->node;
+
+    // If function has some attributes, we need to clone the attributes too
+    auto cloneNode = funcNode;
+    while (cloneNode->parent && cloneNode->parent->kind == AstNodeKind::AttrUse)
+        cloneNode = cloneNode->parent;
+    cloneNode        = cloneNode->clone(cloneContext);
+    auto newFuncNode = cloneNode;
+    while (newFuncNode->kind == AstNodeKind::AttrUse)
+        newFuncNode = newFuncNode->childs.back();
+
+    AstFuncDecl* newFunc = CastAst<AstFuncDecl>(newFuncNode, AstNodeKind::FuncDecl);
+    newFunc->flags |= AST_FROM_GENERIC;
+    newFunc->originalGeneric  = funcNode;
+    newFunc->requestedGeneric = node;
+
+    // If this is for testing a #validif match, we must not evaluate the function content until the
+    // #validif has passed
+    if (validif)
+        newFunc->content->flags |= AST_NO_SEMANTIC;
+    else
+        newFunc->content->flags &= ~AST_NO_SEMANTIC;
+
+    auto p = funcNode->parent;
+    while (p && p->kind == AstNodeKind::AttrUse)
+        p = p->parent;
+    Ast::addChildBack(p, cloneNode);
+
+    // :ContextCall
+    // If we are calling the function in a struct context (struct.func), then add the struct as
+    // an alternative scope
+    if (context->node->kind == AstNodeKind::Identifier)
+    {
+        auto identifier = CastAst<AstIdentifier>(node, AstNodeKind::Identifier);
+        if (identifier->identifierRef()->resolvedSymbolOverload)
+        {
+            auto concreteType = TypeManager::concreteType(identifier->identifierRef()->resolvedSymbolOverload->typeInfo);
+            if (concreteType->isStruct())
+            {
+                auto contextualStruct = CastAst<AstStruct>(concreteType->declNode, AstNodeKind::StructDecl);
+                newFunc->addAlternativeScope(contextualStruct->scope);
+            }
+        }
+    }
+
+    // If we are in a function, inherit also the scopes from the function.
+    // Be carreful that if the call is inside a #macro, we do not want to inherit the function (3550)
+    if (node->ownerFct &&
+        node->ownerFct->hasExtMisc() &&
+        node->ownerFct->extMisc()->alternativeScopes.size() &&
+        !node->findParent(AstNodeKind::CompilerMacro))
+    {
+        newFunc->addAlternativeScopes(node->ownerFct->extMisc()->alternativeScopes);
+    }
+
+    // Inherit alternative scopes from the generic function
+    if (funcNode->hasExtMisc() &&
+        funcNode->extMisc()->alternativeScopes.size())
+    {
+        newFunc->addAlternativeScopes(funcNode->extMisc()->alternativeScopes);
+    }
+
+    // Generate and initialize a new type if the type is still generic
+    // The type is still generic if the doTypeSubstitution didn't find any type to change
+    // (for example if we have just generic value)
+    TypeInfoFuncAttr* newTypeFunc = CastTypeInfo<TypeInfoFuncAttr>(newFunc->typeInfo, newFunc->typeInfo->kind);
+    if (newTypeFunc->isGeneric() || noReplaceTypes)
+    {
+        newTypeFunc = CastTypeInfo<TypeInfoFuncAttr>(newFunc->typeInfo->clone(), newFunc->typeInfo->kind);
+        newTypeFunc->removeGenericFlag();
+        newTypeFunc->declNode      = newFunc;
+        newTypeFunc->replaceTypes  = cloneContext.replaceTypes;
+        newTypeFunc->replaceValues = cloneContext.replaceValues;
+        newTypeFunc->replaceFrom   = cloneContext.replaceFrom;
+        newFunc->typeInfo          = newTypeFunc;
+        if (noReplaceTypes)
+            newTypeFunc->flags |= TYPEINFO_UNDEFINED;
+    }
+    else
+    {
+        newTypeFunc->replaceTypes  = cloneContext.replaceTypes;
+        newTypeFunc->replaceValues = cloneContext.replaceValues;
+        newTypeFunc->replaceFrom   = cloneContext.replaceFrom;
+    }
+
+    // Replace generic types and values in the function generic parameters
+    SWAG_CHECK(updateGenericParameters(context, true, true, newTypeFunc->genericParameters, newFunc->genericParameters->childs, genericParameters, match));
+    newTypeFunc->forceComputeName();
+
+    auto job = end(context, context->baseJob, match.symbolName, cloneNode, true, cloneContext.replaceTypes);
+    context->baseJob->jobsToAdd.push_back(job);
+
+    return true;
+}
+
+bool Generic::instantiateDefaultGenericFunc(SemanticContext* context)
+{
+    auto node = context->node;
+
+    if (node->kind == AstNodeKind::Identifier)
+    {
+        auto identifier = CastAst<AstIdentifier>(node, AstNodeKind::Identifier);
+        if (!identifier->genericParameters)
+        {
+            auto typeFunc = CastTypeInfo<TypeInfoFuncAttr>(node->typeInfo, TypeInfoKind::FuncAttr);
+            auto nodeFunc = CastAst<AstFuncDecl>(typeFunc->declNode, AstNodeKind::FuncDecl);
+            if (nodeFunc->genericParameters)
+            {
+                identifier->typeInfo          = nullptr;
+                identifier->genericParameters = Ast::newFuncCallGenParams(context->sourceFile, identifier);
+                identifier->genericParameters->flags |= AST_NO_BYTECODE;
+                Ast::removeFromParent(identifier->genericParameters);
+                Ast::addChildFront(identifier, identifier->genericParameters);
+
+                CloneContext cloneContext;
+                for (auto p : nodeFunc->genericParameters->childs)
+                {
+                    auto param = CastAst<AstVarDecl>(p, AstNodeKind::FuncDeclParam);
+                    if (!param->assignment)
+                    {
+                        Diagnostic diag{node->sourceFile, node->token, Fmt(Err(Err0715), identifier->resolvedSymbolName->name.c_str())};
+                        diag.hint = Nte(Nte1056);
+                        return context->report(diag, Diagnostic::hereIs(identifier->resolvedSymbolOverload));
+                    }
+
+                    auto child          = Ast::newFuncCallParam(context->sourceFile, identifier->genericParameters);
+                    cloneContext.parent = child;
+                    param->assignment->clone(cloneContext);
+                }
+
+                identifier->identifierRef()->flags &= ~AST_IS_GENERIC;
+                identifier->flags &= ~AST_IS_GENERIC;
+
+                // Force the reevaluation of the identifier and its childs
+                context->result     = ContextResult::NewChilds1;
+                node->semanticState = AstNodeResolveState::Enter;
+                Ast::visit(node, [](AstNode* n)
+                           { n->semFlags &= ~SEMFLAG_ONCE; });
+                return true;
+            }
+        }
+    }
+
+    // :ContextCall
+    // Get the contextual structure call if it exists
+    if (context->node->kind == AstNodeKind::Identifier)
+    {
+        auto identifier = CastAst<AstIdentifier>(node, AstNodeKind::Identifier);
+        auto idRef      = identifier->identifierRef();
+        if (idRef->resolvedSymbolOverload)
+        {
+            auto concreteType = TypeManager::concreteType(idRef->resolvedSymbolOverload->typeInfo);
+            if (concreteType->isStruct())
+            {
+                auto contextualNode = idRef->previousResolvedNode;
+                if (contextualNode)
+                {
+                    Diagnostic diag{node->sourceFile, node->token, Fmt(Err(Err0715), node->token.ctext())};
+                    diag.hint = Nte(Nte1057);
+                    diag.addRange(contextualNode, Nte(Nte1056));
+                    return context->report(diag);
+                }
+            }
+        }
+    }
+
+    return context->report({node->sourceFile, node->token, Fmt(Err(Err0042), node->token.ctext())});
+}
---
+++ src/Generic_Struct.cpp
@@ -0,0 +1,197 @@
+#include "pch.h"
+#include "Generic.h"
+#include "Ast.h"
+#include "Diagnostic.h"
+#include "SemanticJob.h"
+#include "TypeManager.h"
+
+bool Generic::instantiateDefaultGenericVar(SemanticContext* context, AstVarDecl* node)
+{
+    if (node->typeInfo->isStruct() && node->type && node->type->kind == AstNodeKind::TypeExpression)
+    {
+        auto typeExpr = CastAst<AstTypeExpression>(node->type, AstNodeKind::TypeExpression);
+        if (typeExpr->identifier)
+        {
+            auto typeStruct = CastTypeInfo<TypeInfoStruct>(node->typeInfo, TypeInfoKind::Struct);
+            auto nodeStruct = CastAst<AstStruct>(typeStruct->declNode, AstNodeKind::StructDecl);
+            if (nodeStruct->genericParameters)
+            {
+                auto idRef      = CastAst<AstIdentifierRef>(typeExpr->identifier, AstNodeKind::IdentifierRef);
+                auto identifier = CastAst<AstIdentifier>(idRef->childs.back(), AstNodeKind::Identifier);
+                if (!identifier->genericParameters)
+                {
+                    identifier->genericParameters = Ast::newFuncCallGenParams(context->sourceFile, identifier);
+                    identifier->genericParameters->flags |= AST_NO_BYTECODE;
+
+                    CloneContext cloneContext;
+                    for (auto p : nodeStruct->genericParameters->childs)
+                    {
+                        auto param = CastAst<AstVarDecl>(p, AstNodeKind::FuncDeclParam);
+                        if (!param->assignment)
+                        {
+                            Diagnostic diag{node->sourceFile, node->type->token, Fmt(Err(Err0721), typeExpr->identifier->resolvedSymbolName->name.c_str())};
+                            diag.hint = Nte(Nte1056);
+                            return context->report(diag, Diagnostic::hereIs(typeExpr->identifier->resolvedSymbolOverload));
+                        }
+
+                        auto child          = Ast::newFuncCallParam(context->sourceFile, identifier->genericParameters);
+                        cloneContext.parent = child;
+                        param->assignment->clone(cloneContext);
+                    }
+
+                    idRef->flags &= ~AST_IS_GENERIC;
+                    identifier->flags &= ~AST_IS_GENERIC;
+                    node->flags &= ~AST_IS_GENERIC;
+                    node->type->flags &= ~AST_IS_GENERIC;
+
+                    // Force the reevaluation of the variable and its childs
+                    context->result     = ContextResult::NewChilds;
+                    node->semanticState = AstNodeResolveState::Enter;
+                    Ast::visit(node, [](AstNode* n)
+                               { n->semFlags &= ~SEMFLAG_ONCE; });
+                    return true;
+                }
+            }
+        }
+    }
+
+    return context->report({node, Fmt(Err(Err0312), node->typeInfo->getDisplayNameC())});
+}
+
+bool Generic::instantiateStruct(SemanticContext* context, AstNode* genericParameters, OneGenericMatch& match, bool& alias)
+{
+    auto node = context->node;
+    SWAG_VERIFY(!match.genericReplaceTypes.empty(), context->report({node, Fmt(Err(Err0039), node->token.ctext())}));
+
+    // Be sure all methods have been registered, because we need opDrop & co to be known, as we need
+    // to instantiate them also (because those functions can be called by the compiler itself, not by the user)
+    auto typeStruct = CastTypeInfo<TypeInfoStruct>(match.symbolOverload->typeInfo, match.symbolOverload->typeInfo->kind);
+    Semantic::waitAllStructSpecialMethods(context->baseJob, typeStruct);
+    YIELD();
+    Semantic::waitAllStructInterfaces(context->baseJob, typeStruct);
+    YIELD();
+
+    // Clone original node
+    auto overload   = match.symbolOverload;
+    auto sourceNode = overload->node;
+
+    // Can be a type alias
+    // In that case, we need to retrieve the real struct
+    auto genericStructType = CastTypeInfo<TypeInfoStruct>(overload->typeInfo, overload->typeInfo->kind);
+    auto sourceSymbol      = match.symbolName;
+    SWAG_VERIFY(sourceNode->kind == AstNodeKind::StructDecl, context->report({node, node->token, Fmt(Err(Err0040), node->token.ctext())}));
+
+    // Make a new type
+    auto newType = CastTypeInfo<TypeInfoStruct>(genericStructType->clone(), genericStructType->kind);
+    newType->removeGenericFlag();
+    newType->fromGeneric = genericStructType;
+
+    // Replace generic types in the struct generic parameters
+    auto sourceNodeStruct = CastAst<AstStruct>(sourceNode, AstNodeKind::StructDecl);
+    SWAG_CHECK(updateGenericParameters(context, true, false, newType->genericParameters, sourceNodeStruct->genericParameters->childs, genericParameters, match));
+    newType->forceComputeName();
+
+    // Be sure that after the generic instantiation, we will not have a match.
+    // If this is the case, then the corresponding instantiation is used instead of creating a new exact same one.
+    // This can happen when deducing generic parameters.
+    for (auto& v : sourceSymbol->overloads)
+    {
+        if (v->typeInfo->name == newType->name)
+        {
+            TypeInfoStruct* t0 = (TypeInfoStruct*) v->typeInfo;
+            TypeInfoStruct* t1 = (TypeInfoStruct*) newType;
+
+            bool same = true;
+            for (size_t i = 0; i < t0->genericParameters.size(); i++)
+            {
+                if (!t0->genericParameters[i]->typeInfo->isSame(t1->genericParameters[i]->typeInfo, CASTFLAG_EXACT))
+                {
+                    same = false;
+                    break;
+                }
+            }
+
+            if (same)
+            {
+                alias                = true;
+                match.symbolOverload = v;
+                return true;
+            }
+        }
+    }
+
+    CloneContext cloneContext;
+    cloneContext.replaceTypes  = match.genericReplaceTypes;
+    cloneContext.replaceValues = match.genericReplaceValues;
+    cloneContext.replaceFrom   = match.genericReplaceFrom;
+
+    // Add the struct type replacement now, in case the struct has a field to replace
+    cloneContext.replaceTypes[overload->typeInfo->name] = newType;
+
+    // :GenericConcreteAlias
+    // Make all types concrete in case of simple aliases
+    for (auto& p : cloneContext.replaceTypes)
+    {
+        p.second = TypeManager::concreteType(p.second, CONCRETE_ALIAS);
+    }
+
+    auto structNode = CastAst<AstStruct>(sourceNode->clone(cloneContext), AstNodeKind::StructDecl);
+    structNode->flags |= AST_FROM_GENERIC;
+    structNode->content->flags &= ~AST_NO_SEMANTIC;
+    Ast::addChildBack(sourceNode->parent, structNode);
+
+    newType->scope              = structNode->scope;
+    newType->declNode           = structNode;
+    newType->replaceTypes       = cloneContext.replaceTypes;
+    newType->replaceValues      = cloneContext.replaceValues;
+    newType->replaceFrom        = cloneContext.replaceFrom;
+    structNode->typeInfo        = newType;
+    structNode->originalGeneric = sourceNode;
+
+    // Replace generic values in the struct generic parameters
+    SWAG_CHECK(updateGenericParameters(context, false, true, newType->genericParameters, structNode->genericParameters->childs, genericParameters, match));
+
+    auto structJob = end(context, context->baseJob, sourceSymbol, structNode, true, cloneContext.replaceTypes);
+
+    instantiateSpecialFunc(context, structJob, cloneContext, &newType->opUserDropFct);
+    instantiateSpecialFunc(context, structJob, cloneContext, &newType->opUserPostCopyFct);
+    instantiateSpecialFunc(context, structJob, cloneContext, &newType->opUserPostMoveFct);
+
+    // Force instantiation of all special functions
+    for (auto method : newType->methods)
+    {
+        if (method->declNode->isSpecialFunctionName())
+        {
+            auto specFunc = CastAst<AstFuncDecl>(method->declNode, AstNodeKind::FuncDecl);
+            if (specFunc != genericStructType->opUserDropFct &&
+                specFunc != genericStructType->opUserPostCopyFct &&
+                specFunc != genericStructType->opUserPostMoveFct &&
+                !specFunc->genericParameters)
+            {
+                instantiateSpecialFunc(context, structJob, cloneContext, &specFunc);
+            }
+        }
+    }
+
+    // Instantiate generic interfaces
+    structNode->allocateExtension(ExtensionKind::Owner);
+    cloneContext.ownerStructScope   = structNode->scope;
+    newType->cptRemainingInterfaces = (uint32_t) genericStructType->interfaces.size();
+    for (auto itf : genericStructType->interfaces)
+    {
+        auto typeItf = (TypeInfoParam*) itf->clone();
+        newType->interfaces.push_back(typeItf);
+
+        SWAG_ASSERT(itf->declNode);
+        auto newItf       = itf->declNode->clone(cloneContext);
+        typeItf->declNode = newItf;
+        newItf->flags |= AST_FROM_GENERIC;
+        structNode->extOwner()->nodesToFree.push_back(newItf);
+
+        auto implJob = SemanticJob::newJob(context->baseJob->dependentJob, context->sourceFile, newItf, false);
+        structJob->addDependentJob(implJob);
+    }
+
+    context->baseJob->jobsToAdd.push_back(structJob);
+    return true;
+}
