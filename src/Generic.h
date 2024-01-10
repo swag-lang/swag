59fb3b64c5f6f1e2eec42dd8e930e6f0aeb190b7
swag-lang <swaglanggit@gmail.com>
2024-01-10 10:30:31 +0100

    refac

--- src/Generic.h
+++ src/Generic.h
@@ -24,9 +24,9 @@ struct GenericReplaceType
 namespace Generic
 {
     bool      updateGenericParameters(SemanticContext* context, bool doType, bool doNode, VectorNative<TypeInfoParam*>& typeGenericParameters, VectorNative<AstNode*>& nodeGenericParameters, AstNode* callGenericParameters, OneGenericMatch& match);
-    Job*      end(SemanticContext* context, Job* dependentJob, SymbolName* symbol, AstNode* newNode, bool waitSymbol, VectorMap<Utf8, GenericReplaceType>& replaceTypes);
     TypeInfo* doTypeSubstitution(VectorMap<Utf8, GenericReplaceType>& replaceTypes, TypeInfo* typeInfo);
-    void      deduceGenericParam(SymbolMatchContext& context, AstNode* callParameter, TypeInfo* callTypeInfo, TypeInfo* wantedTypeInfo, int idxParam, uint64_t castFlags);
+    Job*      end(SemanticContext* context, Job* dependentJob, SymbolName* symbol, AstNode* newNode, bool waitSymbol, VectorMap<Utf8, GenericReplaceType>& replaceTypes);
+    void      deduceGenericTypeReplacement(SymbolMatchContext& context, AstNode* callParameter, TypeInfo* callTypeInfo, TypeInfo* wantedTypeInfo, int idxParam, uint64_t castFlags);
     void      setContextualGenericTypeReplacement(SemanticContext* context, OneTryMatch& oneTryMatch, SymbolOverload* symOverload, uint32_t flags);
     void      setUserGenericTypeReplacement(SymbolMatchContext& context, VectorNative<TypeInfoParam*>& genericParameters);
 
