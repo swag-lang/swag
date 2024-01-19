cfd0b2e068b71e2fa65e8aff5329d011d9dcad41
swag-lang <swaglanggit@gmail.com>
2024-01-19 08:41:20 +0100

    refac

--- src/SCBE_Coff.h
+++ src/SCBE_Coff.h
@@ -17,11 +17,6 @@ struct SCBE_Coff
 {
     static bool emitHeader(const BuildParameters& buildParameters, SCBE_CPU& pp);
     static void emitUnwind(Concat& concat, uint32_t& offset, uint32_t sizeProlog, const VectorNative<uint16_t>& unwind);
-    static bool emitXData(const BuildParameters& buildParameters, SCBE_CPU& pp);
-    static bool emitPData(const BuildParameters& buildParameters, SCBE_CPU& pp);
-    static bool emitDirectives(const BuildParameters& buildParameters, SCBE_CPU& pp);
-    static bool emitSymbolTable(const BuildParameters& buildParameters, SCBE_CPU& pp);
-    static bool emitStringTable(const BuildParameters& buildParameters, SCBE_CPU& pp);
     static bool emitRelocationTable(Concat& concat, CPURelocationTable& cofftable, uint32_t* sectionFlags, uint16_t* count);
     static bool emitPostFunc(const BuildParameters& buildParameters, SCBE_CPU& pp);
 
