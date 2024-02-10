d8b16c546de064cb246eb2d1fed07bbe9a24b809
swag-lang <swaglanggit@gmail.com>
2024-02-10 08:14:28 +0100

    freetype

--- build/vs_build_extern.bat
+++ build/vs_build_extern.bat
@@ -1,6 +1,6 @@
 call "vs_build_cfg.bat"
 
-%MSBUILD% "..\bin\std\modules\freetype\vendor\freetype-2.10.4\builds\windows\vc2010\freetype.sln" /p:Configuration="Release" -m -verbosity:m
+%MSBUILD% "..\bin\std\modules\freetype\vendor\freetype-2.10.4\builds\windows\vc2010\freetype.sln" /p:Configuration=Release /p:Platform=x64 -m -verbosity:m
 xcopy "..\bin\std\modules\freetype\vendor\freetype-2.10.4\objs\x64\release\freetype.dll" "..\bin\std\modules\freetype\publish\windows-x86_64" /y
 xcopy "..\bin\std\modules\freetype\vendor\freetype-2.10.4\objs\x64\release\freetype.lib" "..\bin\std\modules\freetype\publish\windows-x86_64" /y
 
