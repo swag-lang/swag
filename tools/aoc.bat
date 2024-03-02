2afc3e677dafdd2179b8c88278f35a02c57a0a7d
swag-lang <swaglanggit@gmail.com>
2024-03-02 18:26:56 +0100

    Add Day 5 solution to Advent of Code 2016
    A new SWG file is added to the project, consisting of the code for the solution of Day 5 from Advent of Code 2016. Additionally, MD5 conversion to string functionality is added to the existing crypto library. The run script (aoc.bat) is modified to target 2016 exercises.

--- tools/aoc.bat
+++ tools/aoc.bat
@@ -1,3 +1,3 @@
 REM ..\bin\swag run -w:..\bin\examples -m:aoc2018 --bytecode --cfg:debug %1 %2
-..\bin\swag run -w:..\bin\examples -m:aoc2018 --cfg:debug %1 %2
+..\bin\swag run -w:..\bin\examples -m:aoc2016 --cfg:debug %1 %2
 REM ..\bin\swag run -w:..\bin\examples -m:aoc2020 --cfg:debug %1 %2
