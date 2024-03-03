59557e54726df9f09aaf9b64d2274c14c9e10b56
swag-lang <swaglanggit@gmail.com>
2024-03-03 10:59:47 +0100

    Remove unnecessary debug code from Tokenizer.cpp
    The commit removes a piece of testing code from Tokenizer.cpp which was not necessary for the functioning of the program. This removal leads to cleaner code and less clutter, improving the maintainability of the program.

--- src/Tokenizer.cpp
+++ src/Tokenizer.cpp
@@ -187,8 +187,6 @@ bool Tokenizer::nextToken(TokenParse& tokenParse)
         ///////////////////////////////////////////
         if (SWAG_IS_EOL(c))
         {
-            if(sourceFile->name == "compiler53.swg")
-                int a = 0;
             if (tokenParse.flags.has(TOKEN_PARSE_EOL_BEFORE))
                 tokenParse.flags.add(TOKEN_PARSE_BLANK_LINE_BEFORE);
             else
