4b8a66c9860dee6683185c55262f5d75a94708a6
swag-lang <swaglanggit@gmail.com>
2024-03-08 16:39:19 +0100

    Refactor FormatJob to enhance error handling
    This update refactors the `FormatJob` class, improving the overall error handling process. Specifically, the loading of source files, generation of AST, and the writing of formatted content to file have now clear conditions for errors. This change aims to make the code more robust and easier to debug.

--- src/FormatJob.cpp
+++ src/FormatJob.cpp
@@ -1,8 +1,10 @@
 #include "pch.h"
 #include "FormatJob.h"
+#include "ErrorIds.h"
 #include "FormatAst.h"
 #include "Module.h"
 #include "Parser.h"
+#include "Report.h"
 #include "SyntaxJob.h"
 
 JobResult FormatJob::execute()
@@ -18,22 +20,47 @@ JobResult FormatJob::execute()
         tmpFile.flags.add(FILE_SCRIPT);
     tmpFile.flags.add(FILE_FOR_FORMAT);
 
-    tmpFile.load();
-    if (tmpFile.numErrors)
+    // Load source file
+    if (!tmpFile.load())
         return JobResult::ReleaseJob;
 
+    // Generate AST
     SyntaxContext context;
     Parser        parser;
     parser.setup(&context, &tmpModule, &tmpFile, PARSER_TRACK_FORMAT);
     if (!parser.generateAst())
         return JobResult::ReleaseJob;
 
+    // Format
     FormatAst fmt;
     fmt.fmtFlags.add(FORMAT_FOR_BEAUTIFY);
     fmt.outputNode(tmpFile.astRoot);
 
-    if (g_CommandLine.output)
-        fmt.concat->flushToFile(tmpFile.path);
+    // Write to file
+    if (!g_CommandLine.output)
+        return JobResult::ReleaseJob;
+
+    FILE* f = nullptr;
+    if (fopen_s(&f, tmpFile.path, "wb"))
+    {
+        Report::errorOS(formErr(Err0096, tmpFile.path.c_str()));
+        return JobResult::ReleaseJob;
+    }
+
+    const auto   content = fmt.concat->getUtf8();
+    Vector<Utf8> lines;
+    Utf8::tokenize(content, '\n', lines);
+    content.freeBuffer();
+
+    for (auto& l : lines)
+    {
+        l.trimRight();
+        (void) fwrite(l.data(), 1, l.length(), f);
+        (void) fputc('\n', f);
+    }
+
+    (void) fflush(f);
+    (void) fclose(f);
 
     return JobResult::ReleaseJob;
 }
