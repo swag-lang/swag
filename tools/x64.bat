3fb22dc2a8d245958d42078bba05faa727b747e9
swag-lang <swaglanggit@gmail.com>
2024-03-16 08:12:43 +0100

    Add arguments to script and web call commands
    The scripts.bat and web.bat commands in x64.bat and x64_dm.bat files were updated to also accept arguments. By making these changes, we're giving more options and versatility to users, allowing them to pass in their own inputs when calling these scripts.

--- tools/x64.bat
+++ tools/x64.bat
@@ -2,5 +2,5 @@
 ..\bin\swag test -w:../bin/std       --rebuild %1 %2 %3 %4
 ..\bin\swag test -w:../bin/reference --rebuild %1 %2 %3 %4
 ..\bin\swag test -w:../bin/examples  --rebuild %1 %2 %3 %4
-call scripts.bat --script-run:false
-call web.bat --output-doc:false
+call scripts.bat --script-run:false %1 %2
+call web.bat --output-doc:false %1 %2
--- tools/x64_dm.bat
+++ tools/x64_dm.bat
@@ -3,5 +3,5 @@
 ..\bin\swag_devmode test -w:../bin/std       --rebuild %1 %2 %3 %4
 ..\bin\swag_devmode test -w:../bin/reference --rebuild %1 %2 %3 %4
 ..\bin\swag_devmode test -w:../bin/examples  --rebuild %1 %2 %3 %4
-call scripts_dm.bat --script-run:false
-call web_dm.bat --output-doc:false
+call scripts_dm.bat --script-run:false %1 %2
+call web_dm.bat --output-doc:false %1 %2
