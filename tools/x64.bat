e62828e776fe726421a0010334d6279d33692363
swag-lang <swaglanggit@gmail.com>
2024-03-17 12:17:02 +0100

    Add 'ignore-bad-params' flag and support for extra arguments
    The commit includes the addition of 'ignore-bad-params' flag to 'scripts.bat', 'web.bat', 'scripts_dm.bat', and 'web_dm.bat' calls in the 'x64.bat' and 'x64_dm.bat' files. Also, it extends the number of arguments that can be passed to 'swag doc' and 'swag_devmode doc' in 'web.bat' and 'web_dm.bat' files.

--- tools/x64.bat
+++ tools/x64.bat
@@ -2,5 +2,5 @@
 ..\bin\swag test -w:../bin/std       --rebuild %1 %2 %3 %4
 ..\bin\swag test -w:../bin/reference --rebuild %1 %2 %3 %4
 ..\bin\swag test -w:../bin/examples  --rebuild %1 %2 %3 %4
-call scripts.bat --script-run:false %1 %2
-call web.bat --output-doc:false %1 %2
+call scripts.bat --script-run:false %1 %2 --ignore-bad-params
+call web.bat --output-doc:false %1 %2 --ignore-bad-params
--- tools/x64_dm.bat
+++ tools/x64_dm.bat
@@ -3,5 +3,5 @@
 ..\bin\swag_devmode test -w:../bin/std       --rebuild %1 %2 %3 %4
 ..\bin\swag_devmode test -w:../bin/reference --rebuild %1 %2 %3 %4
 ..\bin\swag_devmode test -w:../bin/examples  --rebuild %1 %2 %3 %4
-call scripts_dm.bat --script-run:false %1 %2
-call web_dm.bat --output-doc:false %1 %2
+call scripts_dm.bat --script-run:false --ignore-bad-params %1 %2
+call web_dm.bat --output-doc:false --ignore-bad-params %1 %2
