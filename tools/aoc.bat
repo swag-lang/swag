506487333273a477fb382833a4ec281d01a5ce80
swag-lang <swaglanggit@gmail.com>
2024-04-12 08:33:06 +0200

    Add day5B function and update script configuration
    A new function, day5B, has been added to the modules/aoc2017. This function implements a different logic in its loop to accomplish its task. In addition, the script configuration in tools/aoc.bat has been updated from debug to release mode.

--- tools/aoc.bat
+++ tools/aoc.bat
@@ -1,3 +1,3 @@
 ..\bin\swag run -w:..\bin\examples -m:aoc2017 --cfg:debug %1 %2
-..\bin\swag run -w:..\bin\examples -m:aoc2017 --cfg:debug %1 %2 --bytecode
+..\bin\swag run -w:..\bin\examples -m:aoc2017 --cfg:release %1 %2 --bytecode
 REM ..\bin\swag run -w:..\bin\examples -m:aoc2020 --cfg:debug %1 %2
