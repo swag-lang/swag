c885df1c767f96acf9f34db4b20e365a9a3c4616
swag-lang <swaglanggit@gmail.com>
2024-04-14 09:15:42 +0200

    Add new test file and modify string parsing
    A new test file (compiler5436.swg) has been added to the test suite. In addition, the parsing of raw strings in SyntaxColor.cpp has been changed for correct color mapping, shifting from using '$' to '#' as a delimiter for raw strings.

--- src/Syntax/SyntaxColor.cpp
+++ src/Syntax/SyntaxColor.cpp
@@ -325,11 +325,11 @@ Utf8 syntaxColor(const Utf8& line, SyntaxColorContext& context)
         }
 
         // Raw string
-        if (c == '$' && *pz == '"')
+        if (c == '#' && *pz == '"')
         {
             result += getColor(mode, SyntaxColor::SyntaxString);
             result += c;
-            while (*pz && (pz[0] != '"' || pz[1] != '$'))
+            while (*pz && (pz[0] != '"' || pz[1] != '#'))
                 result += *pz++;
 
             if (*pz)
