13e13d2eb01d0f02fd359196618c93005a4bfcc5
swag-lang <swaglanggit@gmail.com>
2024-04-17 16:05:48 +0200

    Add thread-safety to flag operations in TypeInfo
    The flag operations methods in TypeInfo class, specifically "hasFlag", "addFlag", and "removeFlag", have been updated to use locks ensuring thread-safety. A new shared mutex "mutexFlags" has been introduced exclusively for these operations. This prevents unexpected behaviors due to concurrent flag-checking or modification tasks.

--- src/Semantic/Type/TypeInfo.h
+++ src/Semantic/Type/TypeInfo.h
@@ -130,9 +130,23 @@ struct TypeInfo
     const TypeInfo* getConstAlias() const;
     TypeInfo*       getConcreteAlias() const;
 
-    bool hasFlag(TypeInfoFlags fl) const { return flags.has(fl); }
-    void addFlag(TypeInfoFlags fl) { flags.add(fl); }
-    void removeFlag(TypeInfoFlags fl) { flags.remove(fl); }
+    bool hasFlag(TypeInfoFlags fl) const 
+    { 
+        SharedLock lk(mutexFlags);
+        return flags.has(fl); 
+    }
+
+    void addFlag(TypeInfoFlags fl) 
+    { 
+        ScopedLock lk(mutexFlags);
+        flags.add(fl); 
+    }
+
+    void removeFlag(TypeInfoFlags fl) 
+    { 
+        ScopedLock lk(mutexFlags);
+        flags.remove(fl); 
+    }
 
     bool isSlice() const { return kind == TypeInfoKind::Slice; }
     bool isInterface() const { return kind == TypeInfoKind::Interface; }
@@ -208,6 +222,7 @@ struct TypeInfo
     const Utf8& computeWhateverNameNoLock(ComputeNameKind nameKind);
 
     mutable SharedMutex mutex;
+    mutable SharedMutex mutexFlags;
 
     Utf8 name;
     Utf8 displayName;
