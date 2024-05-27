dabe931be9780daad3f81235d9f815ea4f6d81dd
cb-dt <christophe.beaudet@gmail.com>
2024-05-27 08:03:19 +0200

    Update build revision to 34
    The build revision number in the Version.h file has been incremented from 33 to 34, to reflect the latest changes to the software build.

--- src/Main/Version.h
+++ src/Main/Version.h
@@ -1,4 +1,4 @@
 #pragma once
 static constexpr int SWAG_BUILD_VERSION  = 0;
-static constexpr int SWAG_BUILD_REVISION = 33;
+static constexpr int SWAG_BUILD_REVISION = 34;
 static constexpr int SWAG_BUILD_NUM      = 0;
