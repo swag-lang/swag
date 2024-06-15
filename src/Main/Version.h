6103af5affb8f3e5d75c7c7c69684850dd86985d
cb-dt <christophe.beaudet@gmail.com>
2024-06-15 07:41:36 +0200

    Update SWAG_BUILD_REVISION to 35

--- src/Main/Version.h
+++ src/Main/Version.h
@@ -1,4 +1,4 @@
 #pragma once
 static constexpr int SWAG_BUILD_VERSION  = 0;
-static constexpr int SWAG_BUILD_REVISION = 34;
+static constexpr int SWAG_BUILD_REVISION = 35;
 static constexpr int SWAG_BUILD_NUM      = 0;
