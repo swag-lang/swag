ca1656325f59f0b1140610ffe150a519443d5f83
cb-dt <christophe.beaudet@gmail.com>
2024-07-04 07:06:10 +0200

    Remove BCI_NOT_PURE flag from ByteCode operations
    The BCI_NOT_PURE flag has been removed from two occurrences of the CopyRBtoRA64 ByteCode operation. This change affects the reserveLinearRegisterRC2 function within the ByteCodeGen_Registers component.

--- src/Backend/ByteCode/Gen/ByteCodeGen_Registers.cpp
+++ src/Backend/ByteCode/Gen/ByteCodeGen_Registers.cpp
@@ -116,9 +116,9 @@ void ByteCodeGen::transformResultToLinear2(const ByteCodeGenContext* context, Re
     {
         RegisterList r0;
         reserveLinearRegisterRC2(context, r0);
-        EMIT_INST2(context, ByteCodeOp::CopyRBtoRA64, r0[0], resultRegisterRC[0])->addFlag(BCI_NOT_PURE);
+        EMIT_INST2(context, ByteCodeOp::CopyRBtoRA64, r0[0], resultRegisterRC[0]);
         if (!onlyOne)
-            EMIT_INST2(context, ByteCodeOp::CopyRBtoRA64, r0[1], resultRegisterRC[1])->addFlag(BCI_NOT_PURE);
+            EMIT_INST2(context, ByteCodeOp::CopyRBtoRA64, r0[1], resultRegisterRC[1]);
         freeRegisterRC(context, resultRegisterRC);
         resultRegisterRC = r0;
     }
