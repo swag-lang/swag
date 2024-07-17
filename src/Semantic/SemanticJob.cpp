bdf0faa2a6bf5ab7fc1d261bb854b930925571a4
cb <christophe.beaudet@gmail.com>
2024-07-17 08:19:04 +0200

    Refactor SemanticJob.cpp and add compiler test
    Re-structured the conditional statements in SemanticJob.cpp for improved readability and efficiency. Prevent unnecessary checking of conditions by adding early return statements. Also, a new compiler test, compiler5482.swg, was added to validate the updates.

--- src/Semantic/SemanticJob.cpp
+++ src/Semantic/SemanticJob.cpp
@@ -31,56 +31,55 @@ bool SemanticJob::spawnJob()
 {
     const auto node = context.node;
 
+    if (!context.canSpawn || node == originalNode)
+        return false;
+    if (node->hasOwnerInline())
+        return false;
+
     // Some nodes need to spawn a new semantic job
-    if (context.canSpawn && node != originalNode)
+    switch (node->kind)
     {
-        switch (node->kind)
+        case AstNodeKind::AttrUse:
+            if (!node->ownerScope->isGlobalOrImpl() || node->hasSpecFlag(AstAttrUse::SPEC_FLAG_GLOBAL))
+                break;
+            [[fallthrough]];
+
+        case AstNodeKind::FuncDecl:
+        case AstNodeKind::StructDecl:
+        case AstNodeKind::InterfaceDecl:
+        case AstNodeKind::VarDecl:
+        case AstNodeKind::ConstDecl:
+        case AstNodeKind::TypeAlias:
+        case AstNodeKind::NameAlias:
+        case AstNodeKind::EnumDecl:
+        case AstNodeKind::CompilerAssert:
+        case AstNodeKind::CompilerPrint:
+        case AstNodeKind::CompilerError:
+        case AstNodeKind::CompilerWarning:
+        case AstNodeKind::CompilerRun:
+        case AstNodeKind::AttrDecl:
+        case AstNodeKind::CompilerIf:
+        case AstNodeKind::Impl:
         {
-            case AstNodeKind::AttrUse:
-                if (!node->ownerScope->isGlobalOrImpl() || node->hasSpecFlag(AstAttrUse::SPEC_FLAG_GLOBAL))
-                    break;
-                [[fallthrough]];
+            // A sub thing can be waiting for the owner function to be resolved.
+            // We inform the parent function that we have seen the sub thing, and that
+            // the attributes context is now fine for it. That way, the parent function can
+            // trigger the resolve of the sub things by just removing AST_NO_SEMANTIC or by hand.
+            ScopedLock lk(node->mutex);
 
-            case AstNodeKind::FuncDecl:
-            case AstNodeKind::StructDecl:
-            case AstNodeKind::InterfaceDecl:
-            case AstNodeKind::VarDecl:
-            case AstNodeKind::ConstDecl:
-            case AstNodeKind::TypeAlias:
-            case AstNodeKind::NameAlias:
-            case AstNodeKind::EnumDecl:
-            case AstNodeKind::CompilerAssert:
-            case AstNodeKind::CompilerPrint:
-            case AstNodeKind::CompilerError:
-            case AstNodeKind::CompilerWarning:
-            case AstNodeKind::CompilerRun:
-            case AstNodeKind::AttrDecl:
-            case AstNodeKind::CompilerIf:
-            case AstNodeKind::Impl:
+            // Flag AST_NO_SEMANTIC must be tested with the node locked, as it can be changed in
+            // registerFuncSymbol by another thread
+            if (!node->hasAstFlag(AST_NO_SEMANTIC) && !node->hasSemFlag(SEMFLAG_FILE_JOB_PASS))
             {
-                // A sub thing can be waiting for the owner function to be resolved.
-                // We inform the parent function that we have seen the sub thing, and that
-                // the attributes context is now fine for it. That way, the parent function can
-                // trigger the resolve of the sub things by just removing AST_NO_SEMANTIC or by hand.
-                ScopedLock lk(node->mutex);
-
-                // Flag AST_NO_SEMANTIC must be tested with the node locked, as it can be changed in
-                // registerFuncSymbol by another thread
-                if (!node->hasAstFlag(AST_NO_SEMANTIC) && !node->hasSemFlag(SEMFLAG_FILE_JOB_PASS))
-                {
-                    SWAG_ASSERT(sourceFile->module == module);
-                    const auto job           = newJob(dependentJob, sourceFile, node, false);
-                    job->context.errCxtSteps = context.errCxtSteps;
-                    g_ThreadMgr.addJob(job);
-                }
-
-                node->addSemFlag(SEMFLAG_FILE_JOB_PASS);
-                nodes.pop_back();
-                return true;
+                SWAG_ASSERT(sourceFile->module == module);
+                const auto job           = newJob(dependentJob, sourceFile, node, false);
+                job->context.errCxtSteps = context.errCxtSteps;
+                g_ThreadMgr.addJob(job);
             }
 
-            default:
-                break;
+            node->addSemFlag(SEMFLAG_FILE_JOB_PASS);
+            nodes.pop_back();
+            return true;
         }
     }
 
