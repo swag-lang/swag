3f2aa0f5685042141a28d0187987b014f2288215
cb <christophe.beaudet@gmail.com>
2024-07-22 07:28:10 +0200

    Update SWAG_BUILD_REVISION to 37
    The SWAG_BUILD_REVISION constant in the Version.h file has been incremented from 36 to 37. This update marks the introduction of the new changes in the software.

--- src/Main/Version.h
+++ src/Main/Version.h
@@ -1,4 +1,4 @@
 #pragma once
 static constexpr int SWAG_BUILD_VERSION  = 0;
-static constexpr int SWAG_BUILD_REVISION = 36;
+static constexpr int SWAG_BUILD_REVISION = 37;
 static constexpr int SWAG_BUILD_NUM      = 0;
