7620461b7ec54b585b5f401963b9dd5351d8a1b7
cb <christophe.beaudet@gmail.com>
2024-07-31 08:47:27 +0200

    Add new compiler tests and correct error message typo
    Added new test cases compiler5498.swg and compiler5499.swg to the compiler testsuite. Also, corrected a minor typo in the diagnostic message for error code Err0082 to improve clarity.

--- src/Report/ErrorIds.cpp
+++ src/Report/ErrorIds.cpp
@@ -205,7 +205,7 @@ void initErrors()
     SWAG_ERROR(Err0079, "exception!                                        $ compile-time execution exception");
     SWAG_ERROR(Err0080, "expected [[%s]]                                   $ expected [[%s]] %s, got [[%s]] instead");
     SWAG_ERROR(Err0081, "expected closing [[;]]                            $ the interface member definition should be closed with [[;]] $ you can instead define a function body if you want to declare a default implementation");
-    SWAG_ERROR(Err0082, "expected function body                            $ the function body starting with [[{]], [[=]] or [[=>]] is expected");
+    SWAG_ERROR(Err0082, "expected function body                            $ a function body starting with [[{]], [[=]] or [[=>]] is expected");
     SWAG_ERROR(Err0083, "failed [[%s]] constraint                          $ the %s [[%s]] can't be used because of a failed [[%s]] constraint");
     SWAG_ERROR(Err0084, "failed generic instantiation                      $ the variable creation failed due to the generic type [[%s]]");
     SWAG_ERROR(Err0085, "failed semantic                                   $ can't resolve this");
