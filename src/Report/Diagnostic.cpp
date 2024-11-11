ea97c68c48f15f6c1e8f0a58e5203becf3082472
cb <christophe.beaudet@gmail.com>
2024-11-11 11:35:44 +0100

    p

--- src/Report/Diagnostic.cpp
+++ src/Report/Diagnostic.cpp
@@ -600,7 +600,7 @@ void Diagnostic::collectRanges()
         fixRange(lineCode, r.startLocation, r.width, '[', ']');
 
         r.mid = r.startLocation.column + r.width / 2;
-        if (r.width && ((r.width & 1) == 0))
+        if (r.mid && r.width && ((r.width & 1) == 0))
             r.mid--;
     }
 }
