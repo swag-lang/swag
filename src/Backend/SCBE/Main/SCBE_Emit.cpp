4ba80f8614b2b8522d6d3f7d6c8cd8f15940b586
cb <christophe.beaudet@gmail.com>
2025-02-06 07:14:15 +0100

    Refactor arithmetic operation handling for FDIV, IMUL, and MUL.
    Unified floating-point and integer handling by introducing dynamic register selection based on operand type. Simplified code paths for these operations, resulting in reduced duplication and improved maintainability.

--- src/Backend/SCBE/Main/SCBE_Emit.cpp
+++ src/Backend/SCBE/Main/SCBE_Emit.cpp
@@ -383,21 +383,15 @@ void SCBE::emitBinOpEqS(SCBE_X64& pp, const ByteCodeInstruction* ip, uint32_t of
         pp.emitSetAddress(CPUReg::RCX, CPUReg::RDI, offsetStack + ip->a.u32);
         pp.emitStore(CPUReg::RCX, 0, CPUReg::RAX, numBits);
     }
-    else if (op == CPUOp::FDIV)
+    else if (op == CPUOp::FDIV || op == CPUOp::IMUL || op == CPUOp::MUL)
     {
-        pp.emitLoad(CPUReg::XMM0, CPUReg::RDI, offsetStack + ip->a.u32, numBits);
-        emitIMMB(pp, ip, CPUReg::XMM1, numBits);
-        pp.emitOp(CPUReg::XMM0, CPUReg::XMM1, op, numBits);
-        pp.emitSetAddress(CPUReg::RAX, CPUReg::RDI, offsetStack + ip->a.u32);
-        pp.emitStore(CPUReg::RAX, 0, CPUReg::XMM0, numBits);
-    }
-    else if (op == CPUOp::IMUL || op == CPUOp::MUL)
-    {
-        pp.emitLoad(CPUReg::RAX, CPUReg::RDI, offsetStack + ip->a.u32, numBits);
-        emitIMMB(pp, ip, CPUReg::RCX, numBits);
-        pp.emitOp(CPUReg::RAX, CPUReg::RCX, op, numBits);
+        const auto r0 = SCBE_CPU::isInt(numBits) ? CPUReg::RAX : CPUReg::XMM0;
+        const auto r1 = SCBE_CPU::isInt(numBits) ? CPUReg::RCX : CPUReg::XMM1;
+        pp.emitLoad(r0, CPUReg::RDI, offsetStack + ip->a.u32, numBits);
+        emitIMMB(pp, ip, r1, numBits);
+        pp.emitOp(r0, r1, op, numBits);
         pp.emitSetAddress(CPUReg::RCX, CPUReg::RDI, offsetStack + ip->a.u32);
-        pp.emitStore(CPUReg::RCX, 0, CPUReg::RAX, numBits);
+        pp.emitStore(CPUReg::RCX, 0, r0, numBits);
     }
     else if (SCBE_CPU::isInt(numBits) && ip->hasFlag(BCI_IMM_B))
     {
