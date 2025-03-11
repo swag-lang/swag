6438c4f967d2263d841c4c2f314a84e91eca470c
cb <christophe.beaudet@gmail.com>
2025-03-11 17:47:28 +0100

    Update aoc.bat to use aoc2024 module with debug config
    Switched the module from aoc2019 to aoc2024 and changed the configuration from release to debug. This ensures compatibility with the latest module version and aligns with the current development environment setup.

--- tools/aoc.bat
+++ tools/aoc.bat
@@ -1,3 +1,3 @@
-..\bin\swag_devmode run -w:..\bin\examples -m:aoc2019 --cfg:release %1 %2 %3 %4
+..\bin\swag_devmode run -w:..\bin\examples -m:aoc2024 --cfg:debug %1 %2 %3 %4
 REM ..\bin\swag run -w:..\bin\examples -m:aoc2017 --cfg:debug %1 %2 %3 %4 --bytecode
 REM ..\bin\swag run -w:..\bin\examples -m:aoc2020 --cfg:debug %1 %2 %3 %4
