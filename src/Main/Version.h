c4a48e6f5a242306155b421b73d854ddc1d03a88
cb <christophe.beaudet@gmail.com>
2025-03-12 16:58:18 +0100

    Update build revision number to 43
    Incremented the SWAG_BUILD_REVISION constant to reflect the latest build version. This ensures versioning aligns with the current development changes.

--- src/Main/Version.h
+++ src/Main/Version.h
@@ -1,4 +1,4 @@
 #pragma once
 static constexpr int SWAG_BUILD_VERSION  = 0;
-static constexpr int SWAG_BUILD_REVISION = 42;
+static constexpr int SWAG_BUILD_REVISION = 43;
 static constexpr int SWAG_BUILD_NUM      = 0;
