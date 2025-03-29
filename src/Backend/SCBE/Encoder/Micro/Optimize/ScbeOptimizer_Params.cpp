4cc854e63ef30c49604869493300c206acde8ce3
cb <christophe.beaudet@gmail.com>
2025-03-29 08:57:00 +0100

    Improve parameter processing in ScbeOptimizer
    Refactored parameter handling to include an early exit when the number of register parameters is exceeded. Introduced a new variable for clarity and updated logic to streamline memory-to-register operations. This ensures cleaner and more efficient code execution.

--- src/Backend/SCBE/Encoder/Micro/Optimize/ScbeOptimizer_Params.cpp
+++ src/Backend/SCBE/Encoder/Micro/Optimize/ScbeOptimizer_Params.cpp
@@ -8,12 +8,14 @@ void ScbeOptimizer::optimizePassParams(const ScbeMicro& out)
 {
     for (uint32_t i = 0; i < out.cc->paramByRegisterInteger.size(); ++i)
     {
-        if (out.cpuFct->typeFunc->numParamsRegisters() > i &&
-            usedRegs[out.cc->paramByRegisterInteger[i]] == 1 &&
+        if (out.cpuFct->typeFunc->numParamsRegisters() <= i)
+            break;
+        const auto r = out.cc->paramByRegisterInteger[i];
+        if (usedRegs[r] == 1 &&
             (!out.cc->useRegisterFloat || !out.cpuFct->typeFunc->registerIdxToType(i)->isNativeFloat()) &&
             !takeAddressRsp.contains(out.cpuFct->getStackOffsetParam(i)))
         {
-            memToReg(out, CpuReg::Rsp, out.cpuFct->getStackOffsetParam(i), out.cc->paramByRegisterInteger[i]);
+            memToReg(out, CpuReg::Rsp, out.cpuFct->getStackOffsetParam(i), r);
         }
     }
 
