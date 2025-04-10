7851b72c5f11517042dfc2c665f1ffb1b1681904
cb <christophe.beaudet@gmail.com>
2025-04-10 08:00:58 +0200

    Improve register allocation logic in ScbeOptimizer.
    Refactored `optimizePassStackToVolatileReg` to prioritize unused volatile and non-volatile registers more effectively. Combined redundant logic to streamline register assignment and ensure better handling of available registers for stack offsets.

--- src/Backend/SCBE/Encoder/Micro/Optimize/ScbeOptimizer_StackToReg.cpp
+++ src/Backend/SCBE/Encoder/Micro/Optimize/ScbeOptimizer_StackToReg.cpp
@@ -27,47 +27,45 @@ void ScbeOptimizer::optimizePassParamsKeepReg(const ScbeMicro& out)
 
 void ScbeOptimizer::optimizePassStackToVolatileReg(const ScbeMicro& out)
 {
-    if (!unusedVolatileInteger.empty() && !usedStack.empty())
+    if ((!unusedVolatileInteger.empty() || !unusedNonVolatileInteger.empty()) && !usedStack.empty())
     {
         std::vector<std::pair<uint32_t, uint32_t>> vec(usedStack.begin(), usedStack.end());
         std::ranges::sort(vec, [](const auto& a, const auto& b) {
             return a.second > b.second;
         });
 
-        for (const auto& offset : vec | std::views::keys)
+        if (!unusedVolatileInteger.empty())
         {
-            if (!out.cpuFct->isStackOffsetLocalParam(offset) && !out.cpuFct->isStackOffsetReg(offset))
-                continue;
-            if (aliasStack.contains(offset))
-                continue;
+            for (const auto& offset : vec | std::views::keys)
+            {
+                if (!out.cpuFct->isStackOffsetLocalParam(offset) && !out.cpuFct->isStackOffsetReg(offset))
+                    continue;
+                if (aliasStack.contains(offset))
+                    continue;
 
-            const auto r = unusedVolatileInteger.first();
-            unusedVolatileInteger.erase(r);
-            memToReg(out, CpuReg::Rsp, offset, r);
-            return;
+                const auto r = unusedVolatileInteger.first();
+                unusedVolatileInteger.erase(r);
+                memToReg(out, CpuReg::Rsp, offset, r);
+                return;
+            }
         }
-    }
 
-    if (!unusedNonVolatileInteger.empty() && !usedStack.empty())
-    {
-        std::vector<std::pair<uint32_t, uint32_t>> vec(usedStack.begin(), usedStack.end());
-        std::ranges::sort(vec, [](const auto& a, const auto& b) {
-            return a.second > b.second;
-        });
-
-        for (const auto& it : vec)
+        if (!unusedNonVolatileInteger.empty())
         {
-            if (it.second < 2)
-                break;
-            if (!out.cpuFct->isStackOffsetLocalParam(it.first) && !out.cpuFct->isStackOffsetReg(it.first))
-                continue;
-            if (aliasStack.contains(it.first))
-                continue;
+            for (const auto& it : vec)
+            {
+                if (it.second < 2)
+                    break;
+                if (!out.cpuFct->isStackOffsetLocalParam(it.first) && !out.cpuFct->isStackOffsetReg(it.first))
+                    continue;
+                if (aliasStack.contains(it.first))
+                    continue;
 
-            const auto r = unusedNonVolatileInteger.first();
-            unusedNonVolatileInteger.erase(r);
-            memToReg(out, CpuReg::Rsp, it.first, r);
-            return;
+                const auto r = unusedNonVolatileInteger.first();
+                unusedNonVolatileInteger.erase(r);
+                memToReg(out, CpuReg::Rsp, it.first, r);
+                return;
+            }
         }
     }
 }
