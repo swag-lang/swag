8b8352d43ff39cd8d183930f7b9ceb4c8d8a17ee
cb <christophe.beaudet@gmail.com>
2025-08-15 13:34:26 +0200

    Revert "Refactor `BitStream` to use explicit `self` references for improved clarity and standardize usage of `#global marked`"
    This reverts commit 7c5a4767e8dcc9a0cdd9ded6bcd6143b68f96129.

--- tools/x64.bat
+++ tools/x64.bat
@@ -4,11 +4,4 @@
 ..\bin\swag test -w:../bin/examples  --rebuild %1 %2 %3 %4
 
 call scripts.bat --script-run:false %1 %2 --ignore-bad-params
-call examples.bat --ignore-bad-params %1 %2
-
-..\bin\swag test -w:../bin/testsuite --rebuild -ve --silent %1 %2 %3 %4
-
-..\bin\swag test -w:../bin/testsuite --rebuild --force-format %1 %2 %3 %4
-..\bin\swag test -w:../bin/std       --rebuild --force-format %1 %2 %3 %4
-..\bin\swag test -w:../bin/reference --rebuild --force-format %1 %2 %3 %4
-..\bin\swag test -w:../bin/examples  --rebuild --force-format %1 %2 %3 %4
+call web.bat --output-doc:false %1 %2 --ignore-bad-params
