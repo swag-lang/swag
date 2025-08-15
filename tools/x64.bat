d04b2c3925bf633daa44db18cf94d76418646858
cb <christophe.beaudet@gmail.com>
2025-08-15 13:34:25 +0200

    Revert "Refactor filesystem modules to use explicit `self` references and standardize `#global marked` usage"
    This reverts commit 8b20e5675188b35312eecde19a20a0b7545a998f.

--- tools/x64.bat
+++ tools/x64.bat
@@ -5,3 +5,10 @@
 
 call scripts.bat --script-run:false %1 %2 --ignore-bad-params
 call examples.bat --ignore-bad-params %1 %2
+
+..\bin\swag test -w:../bin/testsuite --rebuild -ve --silent %1 %2 %3 %4
+
+..\bin\swag test -w:../bin/testsuite --rebuild --force-format %1 %2 %3 %4
+..\bin\swag test -w:../bin/std       --rebuild --force-format %1 %2 %3 %4
+..\bin\swag test -w:../bin/reference --rebuild --force-format %1 %2 %3 %4
+..\bin\swag test -w:../bin/examples  --rebuild --force-format %1 %2 %3 %4
