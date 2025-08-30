6a9db66a80e89445a76ceeeaefda1baa7a5c28ea
cb <christophe.beaudet@gmail.com>
2025-08-30 07:49:20 +0200

    Update: Increment `SWAG_BUILD_REVISION` to 45 in `Version.h`.

--- src/Main/Version.h
+++ src/Main/Version.h
@@ -1,4 +1,4 @@
 #pragma once
 static constexpr int SWAG_BUILD_VERSION  = 0;
-static constexpr int SWAG_BUILD_REVISION = 44;
+static constexpr int SWAG_BUILD_REVISION = 45;
 static constexpr int SWAG_BUILD_NUM      = 0;
